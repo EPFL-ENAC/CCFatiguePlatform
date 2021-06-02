dd9fa00ea9e367d64a07d8306b92662e00bcb54e
scottausor <82100032+scottausor@users.noreply.github.com>
2021-06-02 15:13:25 +0200

    code for aggregated data

--- Fatigue_test_dashboard/aggregate.py
+++ Fatigue_test_dashboard/aggregate.py
@@ -11,30 +11,38 @@ os.chdir(r'/Users/scottmatthewssalmon/Desktop/github/CCFatigue/Data_tests/Aggreg
 filenames = [i for i in glob.glob("*.txt")]
 df = [pd.read_csv(file, sep = "\t", header=0, decimal=',') for file in filenames]
 
-
 #print(filenames)
 
 columns = {'Stress Ratio', 'Reliability Level', 'Stress Level No.', 'Stress Parameter', 'Number of Cycles', 'Residual Strength'}
 agg_stress = []
+agg_n_cycles = []
+
 
 
 #maxStress = max(df[j].Stress)
 
-for i in range (0, 14):
+for i in range (0, 11):
     stress = df[i].Stress
-    max_stress = max.stress
+    max_stress = max(stress)
+    agg_stress.append(max_stress)
 
-
-
-
-
-
-
+    n_cycles = df[i].N
+    max_n_cycles = max(n_cycles)
+    agg_n_cycles.append(max_n_cycles)
 
 
 # %%
 
-print(max_stress)
+print(stress)
+print(agg_stress)
+print(agg_n_cycles)
+
+
+# %%
+R_ratio = 0.1
+rel_level = 50
+
+
 
 
 # %%
