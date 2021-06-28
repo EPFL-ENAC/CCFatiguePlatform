b4b9caf0074acc410792c477f25e599bdf3d83fb
scottausor <82100032+scottausor@users.noreply.github.com>
2021-06-28 17:59:24 +0200

    all plots now in full function form

--- Fatigue_test_dashboard/test_dashboard.py
+++ Fatigue_test_dashboard/test_dashboard.py
@@ -13,6 +13,7 @@ from bokeh.models import ColumnDataSource, HoverTool, LinearColorMapper
 import bokeh.plotting as bk
 from bokeh.transform import linear_cmap
 
+
 # Constantes
 
 DATA_DIRECTORY = '/Volumes/GoogleDrive/.shortcut-targets-by-id/306/FatigueDataPlatform files & data/Data Description/File directory example/'
@@ -23,7 +24,7 @@ LAB = "CCLAB"
 RESEARCHER = 'Vahid'
 
 INTERVAL = 10
-LOOP_SPACING = 10000
+LOOP_SPACING = 1000
 MAGNITUDE = -3
 
 
@@ -34,13 +35,17 @@ MAGNITUDE = -3
 
 data_in = 'STD'
 filename = data_in+'_'+DATE+'_'+TEST_TYPE+'_'+TEST_NUMBER+'.csv'
-
+jsonname =data_in+'_'+DATE+'_'+TEST_TYPE+'_'+TEST_NUMBER+'.json'
 
 
 filepath = os.path.join(DATA_DIRECTORY, LAB, RESEARCHER, TEST_TYPE, DATE, data_in, filename)
+jsonpath = os.path.join(DATA_DIRECTORY, LAB, RESEARCHER, TEST_TYPE, DATE, data_in, jsonname)
 
 df = pd.read_csv(filepath)
+meta_df = pd.read_json(jsonpath)
 ### Importing Hysteresis loops analysis file
+
+
 
 # Data treated in hysteresis analysis
 
@@ -53,35 +58,29 @@ filepath = os.path.join(DATA_DIRECTORY, LAB, RESEARCHER, TEST_TYPE, DATE, data_i
 hyst_df = pd.read_csv(filepath, sep = ',', header = 0)
 
 
-#print(df)
 
 
 # Plotting Stress Strain curves, we use the stress - strain values from the data in standard format
 
 ### We select loops for plotting on an arbitrary basis (subject to modifications)
 #sub_index = [0, 1, 5, 10, 100, 200, 1000, 2000, 10000, 50000, 100000, 500000, 1000000]
-#def calculate_sub_index(hyst_df, INTERVAL):
-'''
-Arguments: N_cycles column of the hyst df
+def calculate_sub_index(hyst_df, INTERVAL, LOOP_SPACING, MAGNITUDE):
+    '''
+    Arguments: N_cycles column of the hyst df
 
-Returns: Subset of index used for plotting curves
+    Returns: Subset of index used for plotting curves
 
-Description:
+    Description:
 
-'''
-n_cycles_max = np.max(hyst_df.n_cycles)
-n_cycles_min = np.min(hyst_df.n_cycles)
+    '''
+    n_cycles_max = np.max(hyst_df.n_cycles)
+    n_cycles_min = np.min(hyst_df.n_cycles)
 
-print(n_cycles_max)
-print(n_cycles_min)
+    sub_index_intermediate = np.geomspace(start = LOOP_SPACING, stop = n_cycles_max, num = INTERVAL)
+    sub_index = np.round(sub_index_intermediate, MAGNITUDE)
+    sub_index[0] = 1
 
-sub_index_intermediate = np.geomspace(start = LOOP_SPACING, stop = n_cycles_max, num = INTERVAL)
-sub_index = np.round(sub_index_intermediate, MAGNITUDE)
-sub_index[0] = 1
-print(n_cycles_max)
-print(n_cycles_min)
-print(sub_index_intermediate)
-print(sub_index)
+    return sub_index
     # create range max_cycles - min cycles (n points log spaced)
     # find closest value from previous range in dataset
     #return sub_index
@@ -95,30 +94,32 @@ print(sub_index)
 #    Description:
 #    '''
 
+def create_sub_hystloops(df, sub_index):
+    nb_curve = len(sub_index)
+    ### Conditional plotting of hysteresis loops - we only plot the loops specified by sub_index
+    sub_hystloops = []
+    for j in range(nb_curve):
+        sub_hystloops_strain = []
+        sub_hystloops_stress = []
+        sub_hystloops_ncycles = []
+        for i in range(len(df)):
+            if df.Machine_N_cycles[i] == sub_index[j]:
+                sub_hystloops_strain.append(df.Machine_Displacement[i])
+                sub_hystloops_stress.append(df.Machine_Load[i])
+                sub_hystloops_ncycles.append(df.Machine_N_cycles[i])
 
-nb_curve = len(sub_index)
-### Conditional plotting of hysteresis loops - we only plot the loops specified by sub_index
-sub_hystloops = []
-for j in range(nb_curve):
-    sub_hystloops_strain = []
-    sub_hystloops_stress = []
-    sub_hystloops_ncycles = []
-    for i in range(len(df)):
-        if df.Machine_N_cycles[i] == sub_index[j]:
-            sub_hystloops_strain.append(df.Machine_Displacement[i])
-            sub_hystloops_stress.append(df.Machine_Load[i])
-            sub_hystloops_ncycles.append(df.Machine_N_cycles[i])
+        # make curve closed // Optional
+        if len(sub_hystloops_ncycles) != 0:
+            sub_hystloops_strain.append(sub_hystloops_strain[0])
+            sub_hystloops_stress.append(sub_hystloops_stress[0])
+            sub_hystloops_ncycles.append(sub_hystloops_ncycles[0])
 
-    # make curve closed // Optional
-    if len(sub_hystloops_ncycles) != 0:
-        sub_hystloops_strain.append(sub_hystloops_strain[0])
-        sub_hystloops_stress.append(sub_hystloops_stress[0])
-        sub_hystloops_ncycles.append(sub_hystloops_ncycles[0])
+        sub_hystloops.append({'n_cycles': sub_hystloops_ncycles,
+                                 'strain': sub_hystloops_strain,
+                                 'stress': sub_hystloops_stress})
 
-    sub_hystloops.append({'n_cycles': sub_hystloops_ncycles,
-                             'strain': sub_hystloops_strain,
-                             'stress': sub_hystloops_stress})
-    #return sub_hystloops
+
+    return sub_hystloops
 
 def plot_select_stress_strain(sub_hystloops):
     stressStrain = figure(title = 'Stress - Strain', plot_width=1200, plot_height=800,
@@ -130,27 +131,28 @@ def plot_select_stress_strain(sub_hystloops):
     for curve in sub_hystloops:
         stressStrain.line(x = 'strain', y = 'stress', source = ColumnDataSource(data = curve))
     show(stressStrain)
-plot_select_stress_strain(sub_hystloops)
+
+
 
 
 
 ### Plotting Stress - Strain (all loops) using data in standard format
 
-total_strain = df.Machine_Displacement
-total_stress = df.Machine_Load
 
-
-total_stress_strain = {'strain': total_strain,
-             'stress': total_stress}
 
 def plot_total_stress_strain(df):
+    total_strain = df.Machine_Displacement
+    total_stress = df.Machine_Load
+
+
+    total_stress_strain = {'strain': total_strain,
+                 'stress': total_stress}
     stress_strain = figure(title = 'Stress - Strain', plot_width=1200, plot_height=800,
                            x_axis_label = "Strain", y_axis_label = "Stress")
     stress_strain.add_tools(HoverTool(tooltips=[("Stress", "@stress"), ("Strain", "@strain")]))
     stress_strain.line(x = 'strain', y = 'stress', source = ColumnDataSource(data = total_stress_strain))
     show(stress_strain)
 
-plot_total_stress_strain(df)
 
 
 
@@ -162,7 +164,7 @@ def plot_Load_curve(df):
     loadCurve.line(df.Machine_N_cycles, df.Machine_Load)
     show(loadCurve)
 
-plot_Load_curve(df)
+
 
 
 
@@ -175,7 +177,7 @@ def plot_Strain_envelope(df):
     strainEnvelope.line(df.Machine_N_cycles, df.Machine_Displacement)
     show(strainEnvelope)
 
-plot_Strain_envelope(df)
+
 
 
 
@@ -197,7 +199,7 @@ def plot_creep(hyst_df):
     creep.line(x = 'n_cycles', y = 'creep', source = ColumnDataSource(data = sub_creep))
     show(creep)
 
-plot_creep(hyst_df)
+
 
 
 
@@ -214,13 +216,19 @@ def calculate_tde(hyst_area):
     return tde
 
 def plot_hystarea(hyst_df):
+    hyst_area = hyst_df.hysteresis_area
+    hyst_n_cycles = hyst_df.n_cycles
+
+
+    sub_hyst = {'n_cycles': hyst_n_cycles,
+                 'area': hyst_area}
     area = figure(title = 'Hysteresis loop area evolution', plot_width=1200, plot_height=800,
                   x_axis_label = "N_cycles", y_axis_label = "Hysteresis area")
     area.add_tools(HoverTool(tooltips=[("area", "@area"), ("Nb. cycles", "@n_cycles")]))
     area.line(x = 'n_cycles', y = 'area', source = ColumnDataSource(data = sub_hyst))
     show(area)
 
-plot_hystarea(hyst_df)
+
 
 
 
@@ -229,15 +237,16 @@ plot_hystarea(hyst_df)
 
 ### Stiffness evolution plot - data from hysteresis analysis
 
-hyst_stiff = hyst_df.stiffness
-hyst_n_cycles = hyst_df.n_cycles
 
-
-sub_hyst = {'n_cycles': hyst_n_cycles,
-             'stiffness': hyst_stiff}
 
 
 def plot_stiffness(hyst_df):
+    hyst_stiff = hyst_df.stiffness
+    hyst_n_cycles = hyst_df.n_cycles
+
+
+    sub_hyst = {'n_cycles': hyst_n_cycles,
+                 'stiffness': hyst_stiff}
     stiff = figure(title = 'Stiffness evolution under cyclic loading', plot_width=1200, plot_height=800,
                    x_axis_label = "N_cycles", y_axis_label = "Stiffness")
     stiff.add_tools(HoverTool(tooltips=[("stiffness", "@stiffness"), ("Nb. cycles", "@n_cycles")]))
@@ -245,8 +254,29 @@ def plot_stiffness(hyst_df):
     show(stiff)
 
 
+def get_r_ratio():
+
+    return
 
 
+def main():
+
+    sub_index = calculate_sub_index(hyst_df, INTERVAL, LOOP_SPACING, MAGNITUDE)
+    sub_hystloops = create_sub_hystloops(df, sub_index)
+    plot_select_stress_strain(sub_hystloops)
+    plot_total_stress_strain(df)
+    plot_Load_curve(df)
+    plot_Strain_envelope(df)
+    plot_creep(hyst_df)
+    plot_hystarea(hyst_df)
+    plot_stiffness(hyst_df)
+    TDE = calculate_tde(hyst_area)
+    print(TDE)
+    return
+
+
+if __name__ == "__main__":
+    main()
 
 
 
