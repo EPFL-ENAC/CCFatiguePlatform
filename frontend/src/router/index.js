eea1fc95f144048d8f513dc798a1cf825a350892
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-06-30 11:13:15 +0200

    [ADD](frontend)→ basic, serving 4 main routes

---
+++ frontend/src/router/index.js
@@ -0,0 +1,42 @@
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import Home from '../views/Home.vue'
+// route level code-splitting
+// this generates a separate chunk (name.[hash].js) for each route
+// which is lazy-loaded when the route is visited.
+const DataBase = () => import(/* webpackChunkName: "database" */ '../views/DataBase.vue')
+const DataUpload = () => import(/* webpackChunkName: "dataupload" */ '../views/DataUpload.vue')
+const About = () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+
+Vue.use(VueRouter)
+
+const routes = [
+  {
+    path: '/',
+    name: 'Home',
+    component: Home
+  },
+  {
+    path: '/database',
+    name: 'DataBase',
+    component: DataBase
+  },
+  {
+    path: '/dataupload',
+    name: 'DataUpload',
+    component: DataUpload
+  },
+  {
+    path: '/about',
+    name: 'About',
+    component: About
+  },
+]
+
+const router = new VueRouter({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  routes
+})
+
+export default router
---
+++ frontend/src/views/Home.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="home">
+    <h1>This is the home page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+}
+</script>
