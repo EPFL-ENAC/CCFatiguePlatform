2aeabdb5152f2c2fa2c6f1c7f98e0bbd79e26d1a
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-07-01 09:49:08 +0200

    [ADD](Primevue)→ + TabMenu to switch the page

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,8 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import PrimeVue from 'primevue/config'
+import 'primevue/resources/themes/saga-blue/theme.css'
+import 'primevue/resources/primevue.min.css'
+import 'primeicons/primeicons.css'
+import 'primeflex/primeflex.css'
 
 Vue.config.productionTip = false
+Vue.use(PrimeVue)
 
 new Vue({
   router,
