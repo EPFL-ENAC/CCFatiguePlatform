85166b4113982078b73b4a8074414b6b8bd86e5a
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-07-02 15:00:10 +0200

    [ADD](CORS FIX)→ while doing dev

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,20 @@
 import json
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+allowed_origins = [
+    '*',
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=allowed_origins,
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
 
 
 @app.get('/experience')
