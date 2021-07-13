c17be1d560a73fc58079d2eb59a5ed3cfd225007
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-07-13 20:14:50 +0200

    [ADD](container layout)→ Home & About pages

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,11 +1,11 @@
 <template>
-  <div class="p-m-3 p-text-left">
+  <v-container class="p-text-left">
     <h1>About</h1>
     <h2>Disclaimer</h2>
     <p>
       The CCFatigue data platform was developped as a service to the research community in the field of composite materials. EPFL and the researchers who performed the experiments presented in the database make no warranties to the accuracy of the information that has been collected. For any questions, comments or suggestions please contact the <a href="mailto:secretariat.cclab@epfl.ch">CCLab information desk</a>.
     </p>
-  </div>
+  </v-container>
 </template>
 
 <script>
