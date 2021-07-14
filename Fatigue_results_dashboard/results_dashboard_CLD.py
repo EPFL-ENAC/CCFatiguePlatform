a2f9ad5cf781f3c6a400a1e6deeb5d8f1fb2b180
scottausor <82100032+scottausor@users.noreply.github.com>
2021-07-14 19:15:20 +0200

    curves are not linked anymore upon plotting

--- Fatigue_results_dashboard/results_dashboard_CLD.py
+++ Fatigue_results_dashboard/results_dashboard_CLD.py
@@ -7,7 +7,7 @@ from bokeh.models import ColumnDataSource, HoverTool
 
 DATA_DIRECTORY = '/Volumes/GoogleDrive/.shortcut-targets-by-id/306/FatigueDataPlatform files & data/CCFatigue/3_CLD'
 file_toplot = 'output.txt'
-
+SUB_INDEX = [1000, 10000, 100000, 1000000, 10000000, 100000000, 1000000000]
 
 def read_CLD_Linear(DATA_DIRECTORY, file_toplot):
     method = 'Linear'
@@ -49,132 +49,125 @@ def read_CLD_Kawai(DATA_DIRECTORY, file_toplot):
     kawai = kawai.fillna('')
     return kawai
 
-def sub_n_linear(linear):
-    sub_range_linear = []
-    sub_mean_linear = []
-    sub_n_cycles_linear = []
-    n = linear.n_cycles[0]
-    for i in range(len(linear)):
-        if linear.n_cycles[i] == n:
-            sub_range_linear.append(linear.stress_range[i])
-            sub_mean_linear.append(linear.stress_mean[i])
-            sub_n_cycles_linear.append(linear.n_cycles[i])
-        else:
-            n = linear.n_cycles[i]
-            sub_range_linear.append(linear.stress_range[i])
-            sub_mean_linear.append(linear.stress_mean[i])
-            sub_n_cycles_linear.append(linear.n_cycles[i])
-
-    return sub_range_linear, sub_mean_linear, sub_n_cycles_linear
 
 
-def sub_n_piecewise_linear(piecewise_linear):
-    sub_range_piecewise_linear = []
-    sub_mean_piecewise_linear = []
-    sub_n_cycles_piecewise_linear = []
-    n = piecewise_linear.n_cycles[0]
-    for i in range(len(piecewise_linear)):
-        if piecewise_linear.n_cycles[i] == n:
-            sub_range_piecewise_linear.append(piecewise_linear.stress_range[i])
-            sub_mean_piecewise_linear.append(piecewise_linear.stress_mean[i])
-            sub_n_cycles_piecewise_linear.append(piecewise_linear.n_cycles[i])
-        else:
-            n = piecewise_linear.n_cycles[i]
-            sub_range_piecewise_linear.append(piecewise_linear.stress_range[i])
-            sub_mean_piecewise_linear.append(piecewise_linear.stress_mean[i])
-            sub_n_cycles_piecewise_linear.append(piecewise_linear.n_cycles[i])
 
-    return sub_range_piecewise_linear, sub_mean_piecewise_linear, sub_n_cycles_piecewise_linear
+def sub_n_linear(linear, SUB_INDEX):
+    sub_linear = []
+    for j in range(len(SUB_INDEX)):
+        sub_range_linear = []
+        sub_mean_linear = []
+        sub_n_cycles_linear = []
+        for i in range(len(linear)):
+            if linear.n_cycles[i] == SUB_INDEX[j]:
+                sub_range_linear.append(linear.stress_range[i])
+                sub_mean_linear.append(linear.stress_mean[i])
+                sub_n_cycles_linear.append(linear.n_cycles[i])
 
-def sub_n_piecewise_nonlinear(piecewise_nonlinear):
-    sub_range_piecewise_nonlinear = []
-    sub_mean_piecewise_nonlinear = []
-    sub_n_cycles_piecewise_nonlinear = []
-    n = piecewise_nonlinear.n_cycles[0]
-    for i in range(len(piecewise_nonlinear)):
-        if piecewise_nonlinear.n_cycles[i] == n:
-            sub_range_piecewise_nonlinear.append(piecewise_nonlinear.stress_range[i])
-            sub_mean_piecewise_nonlinear.append(piecewise_nonlinear.stress_mean[i])
-            sub_n_cycles_piecewise_nonlinear.append(piecewise_nonlinear.n_cycles[i])
-        else:
-            n = piecewise_nonlinear.n_cycles[i]
-            sub_range_piecewise_nonlinear.append(piecewise_nonlinear.stress_range[i])
-            sub_mean_piecewise_nonlinear.append(piecewise_nonlinear.stress_mean[i])
-            sub_n_cycles_piecewise_nonlinear.append(piecewise_nonlinear.n_cycles[i])
-
-    return sub_range_piecewise_nonlinear, sub_mean_piecewise_nonlinear, sub_n_cycles_piecewise_nonlinear
-
-def sub_n_boerstra(boerstra):
-    sub_range_boerstra = []
-    sub_mean_boerstra = []
-    sub_n_cycles_boerstra = []
-    n = boerstra.n_cycles[0]
-    for i in range(len(boerstra)):
-        if boerstra.n_cycles[i] == n:
-            sub_range_boerstra.append(boerstra.stress_range[i])
-            sub_mean_boerstra.append(boerstra.stress_mean[i])
-            sub_n_cycles_boerstra.append(boerstra.n_cycles[i])
-        else:
-            n = boerstra.n_cycles[i]
-            sub_range_boerstra.append(boerstra.stress_range[i])
-            sub_mean_boerstra.append(boerstra.stress_mean[i])
-            sub_n_cycles_boerstra.append(boerstra.n_cycles[i])
-
-    return sub_range_boerstra, sub_mean_boerstra, sub_n_cycles_boerstra
+        sub_linear.append({'n_cycles': sub_n_cycles_linear,
+                     'stress_range': sub_range_linear,
+                     'stress_mean': sub_mean_linear})
+    return sub_linear
 
 
-def sub_n_kawai(kawai):
-    sub_range_kawai = []
-    sub_mean_kawai = []
-    sub_n_cycles_kawai = []
-    n = kawai.n_cycles[0]
-    for i in range(len(kawai)):
-        if kawai.n_cycles[i] == n:
-            sub_range_kawai.append(kawai.stress_range[i])
-            sub_mean_kawai.append(kawai.stress_mean[i])
-            sub_n_cycles_kawai.append(kawai.n_cycles[i])
-        else:
-            n = kawai.n_cycles[i]
-            sub_range_kawai.append(kawai.stress_range[i])
-            sub_mean_kawai.append(kawai.stress_mean[i])
-            sub_n_cycles_kawai.append(kawai.n_cycles[i])
-
-    return sub_range_kawai, sub_mean_kawai, sub_n_cycles_kawai
 
 
-def plot_CLD(sub_range_linear, sub_mean_linear, sub_n_cycles_linear, sub_range_piecewise_linear, sub_mean_piecewise_linear, sub_n_cycles_piecewise_linear, sub_range_piecewise_nonlinear, sub_mean_piecewise_nonlinear, sub_n_cycles_piecewise_nonlinear, sub_range_boerstra, sub_mean_boerstra, sub_n_cycles_boerstra, sub_range_kawai, sub_mean_kawai, sub_n_cycles_kawai):
 
-    sub_linear_cld = {'n_cycles': sub_n_cycles_linear,
-                 'stress_range': sub_range_linear,
-                 'stress_mean': sub_mean_linear}
+def sub_n_piecewise_linear(piecewise_linear, SUB_INDEX):
+    sub_piecewise_linear = []
+    for j in range(len(SUB_INDEX)):
+        sub_range_piecewise_linear = []
+        sub_mean_piecewise_linear = []
+        sub_n_cycles_piecewise_linear = []
+        for i in range(len(piecewise_linear)):
+            if piecewise_linear.n_cycles[i] == SUB_INDEX[j]:
+                sub_range_piecewise_linear.append(piecewise_linear.stress_range[i])
+                sub_mean_piecewise_linear.append(piecewise_linear.stress_mean[i])
+                sub_n_cycles_piecewise_linear.append(piecewise_linear.n_cycles[i])
 
-    sub_piecewise_linear_cld = {'n_cycles': sub_n_cycles_piecewise_linear,
-                 'stress_range': sub_range_piecewise_linear,
-                 'stress_mean': sub_mean_piecewise_linear}
+        sub_piecewise_linear.append({'n_cycles': sub_n_cycles_piecewise_linear,
+                     'stress_range': sub_range_piecewise_linear,
+                     'stress_mean': sub_mean_piecewise_linear})
 
-    sub_piecewise_nonlinear_cld = {'n_cycles': sub_n_cycles_piecewise_nonlinear,
-                 'stress_range': sub_range_piecewise_nonlinear,
-                 'stress_mean': sub_mean_piecewise_nonlinear}
+    return sub_piecewise_linear
 
-    sub_boerstra_cld = {'n_cycles': sub_n_cycles_boerstra,
-                 'stress_range': sub_range_boerstra,
-                 'stress_mean': sub_mean_boerstra}
+def sub_n_piecewise_nonlinear(piecewise_nonlinear, SUB_INDEX):
+    sub_piecewise_nonlinear = []
+    for j in range(len(SUB_INDEX)):
+        sub_range_piecewise_nonlinear = []
+        sub_mean_piecewise_nonlinear = []
+        sub_n_cycles_piecewise_nonlinear = []
+        for i in range(len(piecewise_nonlinear)):
+            if piecewise_nonlinear.n_cycles[i] == SUB_INDEX[j]:
+                sub_range_piecewise_nonlinear.append(piecewise_nonlinear.stress_range[i])
+                sub_mean_piecewise_nonlinear.append(piecewise_nonlinear.stress_mean[i])
+                sub_n_cycles_piecewise_nonlinear.append(piecewise_nonlinear.n_cycles[i])
 
-    sub_kawai_cld = {'n_cycles': sub_n_cycles_kawai,
-                 'stress_range': sub_range_kawai,
-                 'stress_mean': sub_mean_kawai}
+        sub_piecewise_nonlinear.append({'n_cycles': sub_n_cycles_piecewise_nonlinear,
+                     'stress_range': sub_range_piecewise_nonlinear,
+                     'stress_mean': sub_mean_piecewise_nonlinear})
+    return sub_piecewise_nonlinear
+
+def sub_n_boerstra(boerstra, SUB_INDEX):
+    sub_boerstra = []
+    for j in range(len(SUB_INDEX)):
+        sub_range_boerstra = []
+        sub_mean_boerstra = []
+        sub_n_cycles_boerstra = []
+        for i in range(len(boerstra)):
+            if boerstra.n_cycles[i] == SUB_INDEX[j]:
+                sub_range_boerstra.append(boerstra.stress_range[i])
+                sub_mean_boerstra.append(boerstra.stress_mean[i])
+                sub_n_cycles_boerstra.append(boerstra.n_cycles[i])
+
+        sub_boerstra.append({'n_cycles': sub_n_cycles_boerstra,
+                     'stress_range': sub_range_boerstra,
+                     'stress_mean': sub_mean_boerstra})
+    return sub_boerstra
+
+
+def sub_n_kawai(kawai, SUB_INDEX):
+    sub_kawai = []
+    for j in range(len(SUB_INDEX)):
+        sub_range_kawai = []
+        sub_mean_kawai = []
+        sub_n_cycles_kawai = []
+        for i in range(len(kawai)):
+            if kawai.n_cycles[i] == SUB_INDEX[j]:
+                sub_range_kawai.append(kawai.stress_range[i])
+                sub_mean_kawai.append(kawai.stress_mean[i])
+                sub_n_cycles_kawai.append(kawai.n_cycles[i])
+
+        sub_kawai.append({'n_cycles': sub_n_cycles_kawai,
+                     'stress_range': sub_range_kawai,
+                     'stress_mean': sub_mean_kawai})
+
+    return sub_kawai
+
+
+def plot_CLD(sub_linear, sub_piecewise_linear, sub_piecewise_nonlinear, sub_boerstra, sub_kawai):
+
+    #sub_linear_cld = {'n_cycles': sub_n_cycles_linear,
+    #             'stress_range': sub_range_linear,
+    #             'stress_mean': sub_mean_linear}
+
+
 
 
 
     plotCLD = figure(title = 'Constant Life Diagram (CLD)', plot_width=1200, plot_height=800,
                   x_axis_label = "Mean Stress", y_axis_label = "Stress Amplitude")
     plotCLD.add_tools(HoverTool(tooltips=[("N", "@n_cycles"), ("Stress range", "@stress_range"), ("Stress mean", "@stress_mean")]))
-    plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = sub_linear_cld), line_color = 'blue')
-    plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = sub_piecewise_linear_cld), line_color = 'red')
-    plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = sub_piecewise_nonlinear_cld), line_color = 'green')
-    #plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = sub_boerstra_cld))
-    #plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = sub_kawai_cld))
-    print(sub_kawai_cld)
+    for curve in sub_linear:
+        plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = curve), line_color = 'blue')
+    for curve in sub_piecewise_linear:
+        plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = curve), line_color = 'red')
+    for curve in sub_piecewise_nonlinear:
+        plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = curve), line_color = 'green')
+    #for curve in sub_boerstra:
+        #plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = curve))
+    #for curve in sub_kawai:
+        #plotCLD.line(x = 'stress_mean', y = 'stress_range', source = ColumnDataSource(data = curve))
     show(plotCLD)
     return
 
@@ -185,12 +178,12 @@ boerstra = read_CLD_Boerstra(DATA_DIRECTORY, file_toplot)
 kawai = read_CLD_Kawai(DATA_DIRECTORY, file_toplot)
 
 
-sub_range_linear, sub_mean_linear, sub_n_cycles_linear = sub_n_linear(linear)
-sub_range_piecewise_linear, sub_mean_piecewise_linear, sub_n_cycles_piecewise_linear = sub_n_piecewise_linear(piecewise_linear)
-sub_range_piecewise_nonlinear, sub_mean_piecewise_nonlinear, sub_n_cycles_piecewise_nonlinear = sub_n_piecewise_nonlinear(piecewise_nonlinear)
-sub_range_boerstra, sub_mean_boerstra, sub_n_cycles_boerstra = sub_n_boerstra(boerstra)
-sub_range_kawai, sub_mean_kawai, sub_n_cycles_kawai = sub_n_kawai(kawai)
+sub_linear = sub_n_linear(linear, SUB_INDEX)
+sub_piecewise_linear = sub_n_piecewise_linear(piecewise_linear, SUB_INDEX)
+sub_piecewise_nonlinear = sub_n_piecewise_nonlinear(piecewise_nonlinear, SUB_INDEX)
+sub_boerstra = sub_n_boerstra(boerstra, SUB_INDEX)
+sub_kawai = sub_n_kawai(kawai, SUB_INDEX)
 
 
 
-plot_CLD(sub_range_linear, sub_mean_linear, sub_n_cycles_linear, sub_range_piecewise_linear, sub_mean_piecewise_linear, sub_n_cycles_piecewise_linear, sub_range_piecewise_nonlinear, sub_mean_piecewise_nonlinear, sub_n_cycles_piecewise_nonlinear, sub_range_boerstra, sub_mean_boerstra, sub_n_cycles_boerstra, sub_range_kawai, sub_mean_kawai, sub_n_cycles_kawai)
+plot_CLD(sub_linear, sub_piecewise_linear, sub_piecewise_nonlinear, sub_boerstra, sub_kawai)
