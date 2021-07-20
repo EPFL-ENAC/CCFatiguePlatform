63aa5de24103d14b4847907ffffaca521e9ece3e
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-07-20 16:34:20 +0200

    [ADD](CCFatigueAnalysis)→ started with experiment-specifications

--- frontend/src/views/CCFatigueAnalysis.vue
+++ frontend/src/views/CCFatigueAnalysis.vue
@@ -1,13 +1,43 @@
 <template>
-  <v-container>
-    <h1>CCFatigue Analysis</h1>
-    ... will come here.
-  </v-container>
+  <div class="ma-5">
+    <v-skeleton-loader v-if="!dataIsFetched"
+      max-width="100%" height="10rem"
+      type="paragraph" />
+
+    <template v-else>
+      <v-card elevation="0">
+        <v-card-title>
+          Experiment : {{ experience['Experiment Type'] }} test, by {{ experience.Researcher }}, {{ experience.Laboratory }}, {{ experience.Experiment.Date }}
+        </v-card-title>
+      </v-card>
+
+      <experiment-specifications
+        :experience="experience"
+      />
+    </template>
+  </div>
 </template>
 
 <script>
+import { mapState, mapGetters } from 'vuex'
+import ExperimentSpecifications from '@/components/ExperimentSpecifications.vue'
+
 export default {
   name: 'CCFatigueAnalysis',
+  components: {
+    ExperimentSpecifications,
+  },
+  computed: {
+    ...mapState([
+      'experience',
+    ]),
+    ...mapGetters([
+      'dataIsFetched',
+    ]),
+  },
+  created() {
+    this.$store.dispatch('fetchExperience')
+  },
 }
 </script>
 
