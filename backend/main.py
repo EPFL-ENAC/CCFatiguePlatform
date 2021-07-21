66f95181cf27544b883b7d19afac47ed4edb1da7
David Tang <david.tang@epfl.ch>
2021-07-21 16:19:13 +0200

    backend: get experiences

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 import json
 from datetime import date
-from typing import Any
+from typing import Any, List
 
 import uvicorn
 from fastapi import FastAPI, Query
@@ -24,6 +24,14 @@ app.add_middleware(
 )
 
 
+class Experience(BaseModel):
+    id: str
+    laboratory: str
+    researcher: str
+    type: str
+    date: date
+
+
 class Plot(BaseModel):
     stress_strain: Any
     creep: Any
@@ -38,13 +46,19 @@ class Test(BaseModel):
     plot: Plot
 
 
+@app.get('/experiences', response_model=List[Experience])
+async def get_experiences() -> List[Experience]:
+    return []
+
+
 @app.get('/experience/test', response_model=Test)
-async def get_experience(
+async def get_test(
         laboratory: str,
         researcher: str,
         experience_type: str = Query(..., alias='experienceType'),
         date: date = Query(...),
-        test_number: int = Query(..., alias='testNumber', ge=0, lt=1000)):
+        test_number: int = Query(..., alias='testNumber', ge=0, lt=1000)
+) -> Test:
     experience_source_file = '../Preprocessing/vahid_CA_skel.json'
     with open(experience_source_file) as f:
         experience_data = json.load(f)
