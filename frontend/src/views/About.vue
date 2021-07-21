36c36bbeac744314e76279713366fc62b8686352
Samuel Bancal <Samuel.Bancal@epfl.ch>
2021-07-21 18:33:46 +0200

    [UPD](About page)→ as requested

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,10 +1,25 @@
 <template>
   <v-container>
     <h1>About</h1>
-    <h2>Disclaimer</h2>
     <p>
-      The CCFatigue data platform was developped as a service to the research community in the field of composite materials. EPFL and the researchers who performed the experiments presented in the database make no warranties to the accuracy of the information that has been collected. For any questions, comments or suggestions please contact the <a href="mailto:secretariat.cclab@epfl.ch">CCLab information desk</a>.
+      CCFatiguePlatform is an initiative from CCLab at EPFL that aims to develop a open-source web application to harmonize, facilitate storage and analysis of composite materials testing datasets. 
     </p>
+    <p>
+      The web app is under active development and at prototype stage. Please report any bugs as Github Issues in the code repository.
+    </p>
+
+    <h3>Contributors</h3>
+    <ul>
+      <li><a href="https://people.epfl.ch/anastasios.vassilopoulos" target="_blank">Tassos Vassilopoulos</a></li>
+      <li><a href="https://people.epfl.ch/charlotte.weil" target="_blank">Charlie Weil</a></li>
+      <li><a href="https://people.epfl.ch/samuel.bancal" target="_blank">Samuel Bancal</a></li>
+      <li><a href="https://people.epfl.ch/scott.matthewssalmon" target="_blank">Scott Salmon</a></li>
+    </ul>
+    
+    <p class="py-5">
+      <img src="/img/epfl-logo.svg" alt="Logo EPFL, École polytechnique fédérale de Lausanne">
+    </p>
+      
   </v-container>
 </template>
 
