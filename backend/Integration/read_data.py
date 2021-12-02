755738ab135d09d3ebf16972bb9b3d3a92cdb04e
ssenecal <95314184+ssenecal@users.noreply.github.com>
2021-12-02 18:07:38 +0100

    Replace big chunk of CC by iteration on the table object for populating the data.

--- backend/Integration/read_data.py
+++ backend/Integration/read_data.py
@@ -75,55 +75,18 @@ def preprocess_metadata(metadata_file, exp_id, file):
                                                  'Subset Size': str,
                                                  'Step Size': str,
                                              })
-    # Flag the missing value that appear as NaN on Enum types and Int
-    #experiment_meta_data['Experiment Type'] = experiment_meta_data['Experiment Type'].fillna('NO_VALUE')
-    #experiment_meta_data['Fracture Mode'] = experiment_meta_data['Fracture Mode'].fillna('NO_VALUE')
-    #experiment_meta_data['Fatigue Test Type'] = experiment_meta_data['Fatigue Test Type'].fillna('NO_VALUE')
-    #experiment_meta_data['Control mode'] = experiment_meta_data['Control mode'].fillna('NO_VALUE')
-    #experiment_meta_data['Subset Size'] = experiment_meta_data['Subset Size'].fillna(np.nan).replace([np.nan], [None])
+    # Flag the missing value that appear as NaN
     experiment_meta_data = experiment_meta_data.fillna(np.nan).replace([np.nan], [None])
-    #experiment_meta_data['Step Size'] = experiment_meta_data['Step Size'].fillna('')
-    # Flag remaining missing value with 0
-    #experiment_meta_data = experiment_meta_data.fillna(0)
     # Build experiment table objects
     experiment = Experiment()
     experiment.__name__ = file
     # Fill the table object columns from the Dframes
     experiment.id = exp_id # Come from the function input
-    experiment.Laboratory = experiment_meta_data.iloc[0, 0]
-    experiment.Researcher = experiment_meta_data.iloc[0, 1]
-    experiment.Date = experiment_meta_data.iloc[0, 2]
-    experiment.Experiment_Type = experiment_meta_data.iloc[0, 3]
-    experiment.Fracture = experiment_meta_data.iloc[0, 4]
-    experiment.Fracture_Mode = experiment_meta_data.iloc[0, 5]
-    experiment.Initial_Crack_length = experiment_meta_data.iloc[0, 6]
-    experiment.Fatigue_Test_Type = experiment_meta_data.iloc[0, 7]
-    experiment.Measuring_Equipment = experiment_meta_data.iloc[0, 8]
-    experiment.Reliability_Level = experiment_meta_data.iloc[0, 9]
-    experiment.Control_mode = experiment_meta_data.iloc[0, 10]
-    experiment.Title = experiment_meta_data.iloc[0, 11]
-    experiment.Author = experiment_meta_data.iloc[0, 12]
-    experiment.Year = experiment_meta_data.iloc[0, 13]
-    experiment.DOI = experiment_meta_data.iloc[0, 14]
-    experiment.Images_Repository = experiment_meta_data.iloc[0, 15]
-    experiment.Fiber_Material = experiment_meta_data.iloc[0, 16]
-    experiment.Fiber_Geometry = experiment_meta_data.iloc[0, 17]
-    experiment.Area_Density = experiment_meta_data.iloc[0, 18]
-    experiment.Resin = experiment_meta_data.iloc[0, 19]
-    experiment.Hardener = experiment_meta_data.iloc[0, 20]
-    experiment.Mixing_ratio = experiment_meta_data.iloc[0, 21]
-    experiment.Length = experiment_meta_data.iloc[0, 22]
-    experiment.Width = experiment_meta_data.iloc[0, 23]
-    experiment.Thickness = experiment_meta_data.iloc[0, 24]
-    experiment.Curing_Time = experiment_meta_data.iloc[0, 25]
-    experiment.Curing_Temperature = experiment_meta_data.iloc[0, 26]
-    experiment.Curing_Pressure = experiment_meta_data.iloc[0, 27]
-    experiment.Fiber_Content = experiment_meta_data.iloc[0, 28]
-    experiment.Stacking_Sequence = experiment_meta_data.iloc[0, 29]
-    experiment.Temperature = experiment_meta_data.iloc[0, 30]
-    experiment.Humidity = experiment_meta_data.iloc[0, 31]
-    experiment.Subset_Size = experiment_meta_data.iloc[0, 32]
-    experiment.Step_Size = experiment_meta_data.iloc[0, 33]
+    for c in experiment.__table__.c:
+        name = c.name
+        if not (name == 'id' or name == 'parent_id'):
+            setattr(experiment,name,experiment_meta_data.loc[0,name.replace('_',' ')])
+
     # Add to experiment list
     experiments.append(experiment)
 
@@ -133,7 +96,8 @@ def preprocess_metadata(metadata_file, exp_id, file):
     # Transform the text false/true in real boolean
     test_meta_data['Run-out '] = test_meta_data['Run-out '].map(dict(Yes=True, No=False))
     # Management of the missing data
-    test_meta_data['Specimen number'] = test_meta_data['Specimen number'].fillna('')
+    #test_meta_data['Specimen number'] = test_meta_data['Specimen number'].fillna('')
+    test_meta_data = test_meta_data.fillna(np.nan).replace([np.nan], [None])
     # Iterate over the length of test, which correspond to the number of tests in the folder
     for i in range(len(test_meta_data)):
         # Build test objects
@@ -143,12 +107,14 @@ def preprocess_metadata(metadata_file, exp_id, file):
             test.id = 1 # First test object ever
         else:
             test.id = len(tests) + 1 # We assume the specimen order match the row order in the file
+
         test.parent_id = experiment.id # We link to the corresponding parent experiment
-        test.Specimen_number = test_meta_data.iloc[i, 0]
-        test.Stress_Ratio = test_meta_data.iloc[i, 1]
-        test.Maximum_Stress = test_meta_data.iloc[i, 2]
-        test.Loading_rate = test_meta_data.iloc[i, 3]
-        test.Run_out = test_meta_data.iloc[i, 4]
+        for c in test.__table__.c:
+            name = c.name
+            if not (name == 'id' or name == 'parent_id' or name == 'Run_out'):
+                setattr(test, name, test_meta_data.loc[i, name.replace('_', ' ')])
+        setattr(test, 'Run_out', test_meta_data.loc[i, 'Run-out'])
+
         # Add to test list above
         tests.append(test)
         tests_local.append(test)
@@ -179,17 +145,7 @@ def preprocess_file(data_file, file):
                                             'Th_lowergrips': str,
                                         })
     # Managing missing data that appear as NaN
-    test_results_data['Machine_N_cycles'] = test_results_data['Machine_N_cycles'].fillna('0')
-    test_results_data['index'] = test_results_data['index'].fillna('0')
-    test_results_data['Camera_N_cycles'] = test_results_data['Camera_N_cycles'].fillna('0')
-    test_results_data['Th_time'] = test_results_data['Th_time'].fillna('0')
-    test_results_data['Th_N cycles'] = test_results_data['Th_N_cycles'].fillna('0')
-    test_results_data['Th_specimen max'] = test_results_data['Th_specimen_max'].fillna('0')
-    test_results_data['Th_specimen mean'] = test_results_data['Th_specimen_mean'].fillna('0')
-    test_results_data['Th_chamber'] = test_results_data['Th_chamber'].fillna('0')
-    test_results_data['Th_uppergrips'] = test_results_data['Th_uppergrips'].fillna('0')
-    test_results_data['Th_lowergrips'] = test_results_data['Th_lowergrips'].fillna('0')
-    test_results_data = test_results_data.fillna('0')
+    test_results_data = test_results_data.fillna(np.nan).replace([np.nan], [None])
     # Getting the test number from the file name
     test_number = int(file.split('_')[-1].split('.')[0])
     # Look for matching folder number given the specimen number
@@ -203,25 +159,12 @@ def preprocess_file(data_file, file):
         test_results = Test_results()
         # Fill the table class from the dataframes
         test_results.parent_id = test_parent.id
-        test_results.Machine_N_cycles = test_results_data.iloc[i, 0]
-        test_results.Machine_Load = test_results_data.iloc[i, 1]
-        test_results.Machine_Displacement = test_results_data.iloc[i, 2]
-        test_results.index = test_results_data.iloc[i, 3]
-        test_results.Camera_N_cycles = test_results_data.iloc[i, 4]
-        test_results.exx = test_results_data.iloc[i, 5]
-        test_results.eyy = test_results_data.iloc[i, 6]
-        test_results.exy = test_results_data.iloc[i, 7]
-        test_results.crack_length = test_results_data.iloc[i, 8]
-        test_results.Th_time = test_results_data.iloc[i, 9]
-        test_results.Th_N_cycles = test_results_data.iloc[i, 10]
-        test_results.Th_specimen_max = test_results_data.iloc[i, 11]
-        test_results.Th_specimen_mean = test_results_data.iloc[i, 12]
-        test_results.Th_chamber = test_results_data.iloc[i, 13]
-        test_results.Th_uppergrips = test_results_data.iloc[i, 14]
-        test_results.Th_lowergrips = test_results_data.iloc[i, 15]
+        for c in test_results.__table__.c:
+            name = c.name
+            if not (name == 'id' or name == 'parent_id'):
+                setattr(test_results, name, test_results_data.loc[0, name.replace('_', ' ')])
         # Add to list above
         test_results_list.append(test_results)
-
 
 # Go through folder hierarchy and analyse files
 def preprocess_data(folder_name):
@@ -265,3 +208,6 @@ def preprocess_data(folder_name):
                                 print('File skipped')
 
     return [experiments, tests, test_results_list]
+
+folder = "../../Data"
+preprocess_data(folder)
