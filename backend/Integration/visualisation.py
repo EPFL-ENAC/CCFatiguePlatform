750102bf72e1bde898e4018dea071964ab2b86f7
ssenecal <95314184+ssenecal@users.noreply.github.com>
2021-12-02 17:13:41 +0100

    Replace the loop by proper query. Put all graph in one object. Add saving to external file.

--- backend/Integration/visualisation.py
+++ backend/Integration/visualisation.py
@@ -1,10 +1,10 @@
 import matplotlib.pyplot as plt
 import pandas
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-from init_db import Test, Test_results
+from init_db import Test, Test_results, Experiment
 
 Base = declarative_base()
 DBSession = scoped_session(sessionmaker())
@@ -24,61 +24,44 @@ def Graphs():
     Session = sessionmaker(bind=engine)
     session = Session()
 
-    ## Visualizing the Stress Ratio distribution
-    StressRatios = []
-    SR_list = []
+    fig = plt.figure(constrained_layout=True)
+    subfigs = fig.subfigures(nrows=2, ncols=1)
 
-    # Pulling the data by filtering per experiment
-    # Iteration on the experiment
+    records = session.query(Test.Stress_Ratio, Experiment.id).join(Experiment).all()
+    df = pandas.DataFrame(records)
+
+    axs = subfigs[0].subplots(nrows=1, ncols=4)
+    subfigs[0].suptitle('Stress Ratio')
     for x in range(1,5):
-        # Iteration on the test list child of the experiment
-        for record in session.query(Test).filter(Test.parent_id == x):
-            # Adding value to a list per experiment
-            StressRatios.append(record.Stress_Ratio)
-        # Collection of the experiment lists
-        SR_list.append(pandas.DataFrame(StressRatios))
+        axs[x-1].hist(df.groupby(1)[0].apply(list).get(x), bins = 15)
+        axs[x-1].set_title("Exp  " + str(x))
+        axs[x - 1].set_xlabel('Value')
+        axs[x - 1].set_ylabel('Frequency')
 
-    fig, axs = plt.subplots(2, 2)
-    fig.suptitle('Stress Ratio')
-    axs[0, 0].hist(SR_list[0])
-    axs[0, 0].set_title("Exp 1")
-    axs[1, 0].hist(SR_list[1])
-    axs[1, 0].set_title("Exp 2")
-    axs[0, 1].hist(SR_list[2])
-    axs[0, 1].set_title("Exp 3")
-    axs[1, 1].hist(SR_list[3])
-    axs[1, 1].set_title("Exp 4")
-    StressRatios.clear()
-    plt.show()
 
     ## Visualizing the Maximum machine load distribution
 
-    ML_list = []
-    MachLoad = []
-    MaxMachLoad = []
     # Pulling the data by filtering per experiment
     # Iteration per experiment
+    records2 = session.query(func.max(Test_results.Machine_Load), Test.id, Experiment.id)\
+                .select_from(Test_results)\
+                .join(Test)\
+                .join(Experiment)\
+                .group_by(Test.id, Experiment.id)\
+                .all()
+
+    df = pandas.DataFrame(records2)
+
+    axs2 = subfigs[1].subplots(nrows=1, ncols=4)
+    subfigs[1].suptitle('Maximum Machine Load')
     for x in range(1,5):
-        # Iteration on the test list child of the experiment
-        for record in session.query(Test).filter(Test.parent_id == x):
-            # Iteration on the test_results list child of the test
-            for record_tr in session.query(Test_results).filter(Test_results.parent_id == record.id):
-                # Adding value to a list per test
-                MachLoad.append(record_tr.Machine_Load)
-            # Taking max value in a list per experiment
-            MaxMachLoad.append(max(MachLoad))
-        ML_list.append(pandas.DataFrame(MaxMachLoad))
-    fig, axs = plt.subplots(2, 2)
-    fig.suptitle('Machine load')
-    axs[0, 0].hist(ML_list[0])
-    axs[0, 0].set_title("Exp 1")
-    axs[1, 0].hist(ML_list[1])
-    axs[1, 0].set_title("Exp 2")
-    axs[0, 1].hist(ML_list[2])
-    axs[0, 1].set_title("Exp 3")
-    axs[1, 1].hist(ML_list[3])
-    axs[1, 1].set_title("Exp 4")
-    MachLoad.clear()
+        axs2[x-1].hist(df.groupby(2)[0].apply(list).get(x), bins= 50, color= 'red')
+        axs2[x-1].set_title("Exp  " + str(x))
+        axs2[x - 1].set_xlabel('Value')
+        axs2[x - 1].set_ylabel('Frequency')
+
+    fig.savefig('StressRatio_MaxMachload_distribution.png')
     plt.show()
+
 
 Graphs()
