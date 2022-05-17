1fb4d787422b59a3bea4c0c08f51195f9de32ca2
Nicolas Dubois <nicolas.dubois@epfl.ch>
2022-05-17 15:18:25 +0200

    Code refactoring LogLog

--- CCFatigue_modules/2_S-NCurves/S-N-Curve-LogLog2.py
+++ CCFatigue_modules/2_S-NCurves/S-N-Curve-LogLog2.py
@@ -9,14 +9,12 @@ TODO
 
 import math
 import os
-from re import A
-from statistics import variance
 import numpy as np
 import pandas as pd
 from scipy import stats
 from itertools import chain
 
-import Astm
+from astm import Astm
 
 
 CONFIDENCE = 95  # TODO check with Tassos if this should be a parameter (95, 99)
@@ -24,12 +22,9 @@ RELIABILITY_LEVEL = 50
 
 SRC_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(SRC_DIR, "..", "..", "Data")
-# PARAM_DIR = os.path.join(SRC_DIR, "..", "ExternalDataSources")
 
-# PARAM_ASTM_FILENAME = "astm.csv" # Used only in Tassos commented code
-# PARAM_ASTM_RELIABILITY_LEVEL_FILENAME = f"astm{CONFIDENCE}.csv"
 
-astm = Astm.Astm(CONFIDENCE)
+astm = Astm(CONFIDENCE)
 
 INPUT_FILENAME = "AGG_input.csv"
 INPUT_FILE = os.path.join(DATA_DIR, INPUT_FILENAME)
@@ -51,59 +46,55 @@ def stress_at_failure(aa, bb, nn):
     return spn
 
 
-def get_stress_at_failure(nod, q, a, b, nn, pp, xb):
+def stress_at_failure_bounds(sample_count, q, a, b, nn, pp, xb):
     """Get Stress at failure (sigma_max)
     returns (lower bound, upper bound)"""
 
-    termi = (nod * q * a * b) - (nod * q * math.log10(nn) * b) + (pp * nod * xb)
-    termii = math.sqrt(
-        nod
+    term1 = (
+        (sample_count * q * a * b)
+        - (sample_count * q * math.log10(nn) * b)
+        + (pp * sample_count * xb)
+    )
+    term2 = math.sqrt(
+        sample_count
         * pp
         * q
         * (
-            2 * nod * a * b * xb
-            - 2 * nod * math.log10(nn) * b * xb
+            2 * sample_count * a * b * xb
+            - 2 * sample_count * math.log10(nn) * b * xb
             + q * b**2
-            + nod * b**2 * xb**2
-            + nod * math.log10(nn) ** 2
-            - 2 * nod * math.log10(nn) * a
+            + sample_count * b**2 * xb**2
+            + sample_count * math.log10(nn) ** 2
+            - 2 * sample_count * math.log10(nn) * a
             - pp
-            + nod * a**2
+            + sample_count * a**2
         )
     )
-    termiii = nod * (b**2 * q - pp)
+    term3 = sample_count * (b**2 * q - pp)
 
-    spnu = 10 ** (-(termi + termii) / termiii)
-    spnl = 10 ** (-(termi - termii) / termiii)
+    stress_upperbound = 10 ** (-(term1 + term2) / term3)
+    stress_lowerbound = 10 ** (-(term1 - term2) / term3)
 
-    return (spnl, spnu)
+    return (stress_lowerbound, stress_upperbound)
 
-
-# RROUT = 1e7
-
-# Import ASTM files
-# astm_df = pd.read_csv(os.path.join(PARAM_DIR, PARAM_ASTM_FILENAME))
-# astm_rl_df = pd.read_csv(os.path.join(PARAM_DIR, PARAM_ASTM_RELIABILITY_LEVEL_FILENAME))
 
 # Import input file (SNC format)
-df = pd.read_csv(INPUT_FILE)
+samples = pd.read_csv(INPUT_FILE)
 
 # Add columns with log10(stress_parameter) and log10(number_of_cycles)
-df["log10_stress_parameter"] = np.log10(df.stress_parameter)
-df["log10_number_of_cycles"] = np.log10(df.number_of_cycles)
+samples["log10_stress_parameter"] = np.log10(samples.stress_parameter)
+samples["log10_number_of_cycles"] = np.log10(samples.number_of_cycles)
 
+# Data are grouped by stress_ratio but one experiment
+# can have two separate groups with same stress_ratio so we need to identify
+samples["stress_ratio_id"] = (
+    samples.stress_ratio != samples.stress_ratio.shift()
+).cumsum()
 
-# TODO: Check with Tassos if input file can have different section with same
-# stress_ratio
-df["stress_ratio_id"] = (df.stress_ratio != df.stress_ratio.shift()).cumsum()
-
-# TODO: Check with Tassos if input file can have different section with same
-# stress_level
-df["stress_level_id"] = (df.stress_level != df.stress_level.shift()).cumsum()
 
 # Average by stress_ratio
-results = (
-    df[
+stress_ratios_df = (
+    samples[
         [
             "stress_ratio_id",
             "stress_ratio",
@@ -138,91 +129,102 @@ snc_output_csv_df = pd.DataFrame(
 )
 
 # Calculate A (Eq 4) and B (Eq 5)
-linregress = df.groupby("stress_ratio_id").apply(
+linregress = samples.groupby("stress_ratio_id").apply(
     lambda x: stats.linregress(x.log10_stress_parameter, x.log10_number_of_cycles)
 )
 
+# Eq 4 and 5
 # Add A and B in avg_by_stress_ratio
 # Slope = A, Intercept = B
-results["slope"] = linregress.apply(lambda x: x[1])
-results["intercept"] = linregress.apply(lambda x: x[0])
-# for stress_ratio_id in df.stress_ratio_id.unique():
-#     df.loc[df.stress_ratio_id == stress_ratio_id, "slope"] = linregress[
-#         stress_ratio_id
-#     ][1]
-#     df.loc[df.stress_ratio_id == stress_ratio_id, "intercept"] = linregress[
-#         stress_ratio_id
-#     ][0]
+stress_ratios_df["slope"] = linregress.apply(lambda x: x[1])
+stress_ratios_df["intercept"] = linregress.apply(lambda x: x[0])
 
 # Add AA and BB TODO reference for these equations
-results["stress_parameter_aa"] = 10 ** (-results.slope / results.intercept)
-results["stress_parameter_bb"] = -1 / results.intercept
+stress_ratios_df["stress_parameter_aa"] = 10 ** (
+    -stress_ratios_df.slope / stress_ratios_df.intercept
+)
+stress_ratios_df["stress_parameter_bb"] = -1 / stress_ratios_df.intercept
 
-df["ycar"] = df.apply(
+samples["ycar"] = samples.apply(
     lambda x: equation_ycar(
-        results.loc[x.stress_ratio_id].slope,
-        results.loc[x.stress_ratio_id].intercept,
+        stress_ratios_df.loc[x.stress_ratio_id].slope,
+        stress_ratios_df.loc[x.stress_ratio_id].intercept,
         x.log10_stress_parameter,
     ),
     axis=1,
 )
 
 # LNb
-results["log10_number_of_cycles"] = (
-    df[["stress_ratio_id", "log10_number_of_cycles"]].groupby("stress_ratio_id").mean()
+stress_ratios_df["log10_number_of_cycles"] = (
+    samples[["stress_ratio_id", "log10_number_of_cycles"]]
+    .groupby("stress_ratio_id")
+    .mean()
 )
 
 # LSST
-df["lsst"] = df.apply(
+samples["lsst"] = samples.apply(
     lambda x: (
-        x.log10_stress_parameter - results.loc[x.stress_ratio_id].log10_number_of_cycles
+        x.log10_stress_parameter
+        - stress_ratios_df.loc[x.stress_ratio_id].log10_number_of_cycles
     )
     ** 2,
     axis=1,
 )
-results["lsst"] = df[["stress_ratio_id", "lsst"]].groupby("stress_ratio_id").sum()
+stress_ratios_df["lsst"] = (
+    samples[["stress_ratio_id", "lsst"]].groupby("stress_ratio_id").sum()
+)
 
+# Eq 9
 # LSSE
-df["lsse"] = (df.log10_number_of_cycles - df.ycar) ** 2
-results["lsse"] = df[["stress_ratio_id", "lsse"]].groupby("stress_ratio_id").sum()
+samples["lsse"] = (samples.log10_number_of_cycles - samples.ycar) ** 2
+stress_ratios_df["lsse"] = (
+    samples[["stress_ratio_id", "lsse"]].groupby("stress_ratio_id").sum()
+)
 
 # LRSQ
-results["lrsq"] = 1 - results.lsse / results.lsst
+stress_ratios_df["lrsq"] = 1 - stress_ratios_df.lsse / stress_ratios_df.lsst
 
 # Q
-results["xb"] = (
-    df[["stress_ratio_id", "log10_stress_parameter"]].groupby("stress_ratio_id").mean()
+stress_ratios_df["xb"] = (
+    samples[["stress_ratio_id", "log10_stress_parameter"]]
+    .groupby("stress_ratio_id")
+    .mean()
 )
-df["q"] = df.apply(
-    lambda x: (x.log10_stress_parameter - results.loc[x.stress_ratio_id].xb) ** 2,
+samples["q"] = samples.apply(
+    lambda x: (x.log10_stress_parameter - stress_ratios_df.loc[x.stress_ratio_id].xb)
+    ** 2,
     axis=1,
 )
-results["q"] = df[["stress_ratio_id", "q"]].groupby("stress_ratio_id").sum()
+stress_ratios_df["q"] = (
+    samples[["stress_ratio_id", "q"]].groupby("stress_ratio_id").sum()
+)
 
 # NOD
-results["nod"] = (
-    df[["stress_ratio_id", "stress_ratio"]].groupby("stress_ratio_id").count()
+stress_ratios_df["sample_count"] = (
+    samples[["stress_ratio_id", "stress_ratio"]].groupby("stress_ratio_id").count()
 )
 
 # level
-results["level"] = (
-    df[["stress_ratio_id", "stress_level"]].groupby("stress_ratio_id").nunique()
+stress_ratios_df["level"] = (
+    samples[["stress_ratio_id", "stress_level"]].groupby("stress_ratio_id").nunique()
 )
 
 
+# Eq 6
 # Variance
-results["variance"] = results.apply(lambda x: math.sqrt(x.lsse / (x.nod - 2)), axis=1)
+stress_ratios_df["variance"] = stress_ratios_df.apply(
+    lambda x: math.sqrt(x.lsse / (x.sample_count - 2)), axis=1
+)
 
 
 # Fp
-results["fp"] = results.apply(
-    # lambda x: astm_rl_df.loc[astm_rl_df.n == x.nod - x.level][str(int(x.level - 2))],
-    lambda x: astm.get_astm_val(x.level - 2, x.nod - x.level),
+stress_ratios_df["fp"] = stress_ratios_df.apply(
+    lambda x: astm.get_astm_val(x.level - 2, x.sample_count - x.level),
     axis=1,
 )
 
 # PP
-results["pp"] = 2 * results.fp * results.variance**2
+stress_ratios_df["pp"] = 2 * stress_ratios_df.fp * stress_ratios_df.variance**2
 
 
 # Prepare SNC output list of cycles to failure
@@ -234,9 +236,6 @@ snc_output_json_df = pd.DataFrame(
         "A",
         "B",
         "Fp",
-        "Linearity",
-        "Sstar",
-        "Cstar",
     ],
 )
 
@@ -251,127 +250,57 @@ snc_output_csv_df = pd.DataFrame(
 )
 
 # For each group of stress_ratio
-for stress_ratio_id in df["stress_ratio_id"].unique():
+for stress_ratio_id in samples["stress_ratio_id"].unique():
 
-    stress_ratio_df = df.loc[df.stress_ratio_id == stress_ratio_id]
-    stress_ratio = stress_ratio_df.iloc[0].stress_ratio
-    result = results.loc[stress_ratio_id]
+    stress_ratio_sample_df = samples.loc[samples.stress_ratio_id == stress_ratio_id]
+    stress_ratio = stress_ratio_sample_df.iloc[0].stress_ratio
+    stress_ratio_df = stress_ratios_df.loc[stress_ratio_id]
 
+    # Prepare CSV data for each cycles to failure
     stress_parameter = cycles_to_failure.copy()
     stress_parameter["stress_parameter"] = stress_parameter.apply(
         lambda x: stress_at_failure(
-            result.stress_parameter_aa, result.stress_parameter_bb, x
+            stress_ratio_df.stress_parameter_aa, stress_ratio_df.stress_parameter_bb, x
         )
     )
 
     stress_bounds = stress_parameter.apply(
-        lambda x: get_stress_at_failure(
-            result.nod,
-            result.q,
-            result.slope,
-            result.intercept,
+        lambda x: stress_at_failure_bounds(
+            stress_ratio_df.sample_count,
+            stress_ratio_df.q,
+            stress_ratio_df.slope,
+            stress_ratio_df.intercept,
             x.cycles_to_failure,
-            result.pp,
-            result.xb,
+            stress_ratio_df.pp,
+            stress_ratio_df.xb,
         ),
         axis=1,
     )
-    stress_parameter["spnu"] = stress_bounds.apply(lambda x: x[1])
-    stress_parameter["spnl"] = stress_bounds.apply(lambda x: x[0])
+    stress_parameter["stress_lowerbound"] = stress_bounds.apply(lambda x: x[0])
+    stress_parameter["stress_upperbound"] = stress_bounds.apply(lambda x: x[1])
 
     stress_parameter["stress_ratio"] = stress_ratio
 
     snc_output_csv_df = pd.concat([snc_output_csv_df, stress_parameter])
 
-#     json_df = pd.DataFrame(
-#         {
-#             "stress_ratio": stress_ratio,
-#             "RSQL": RELIABILITY_LEVEL,
-#             "A": results.stress_parameter_aa,
-#             "B": results.stress_parameter_bb,
-#             "Sstar": results[stress_ratio_id].lrsq,
-#             # "Cstar": c_star,
-#         },
-#         index=[0],
-#     )
-#     snc_output_json_df = pd.concat([snc_output_json_df, json_df], ignore_index=True)
-results["RSQL"] = RELIABILITY_LEVEL
-# results[["stress_ratio", "RSQL", "stress_parameter_aa"]].to_json(orient="records", colu)
+    # Prepare JSON
+    json_df = pd.DataFrame(
+        {
+            "stress_ratio": stress_ratio,
+            "RSQL": RELIABILITY_LEVEL,
+            "A": stress_ratio_df.stress_parameter_aa,
+            "B": stress_ratio_df.stress_parameter_bb,
+            "Fp": stress_ratio_df.fp,
+        },
+        index=[0],
+    )
+    snc_output_json_df = pd.concat([snc_output_json_df, json_df], ignore_index=True)
 
 
-pass
-# LRSQ=1-LSSE/LSST
-# Fp
-# Linearcheck = (FN / (level(f) - 2)) / (FD / (NOD(f) - level(f)))
-# RMSE = sqrt(SSE / NOD(f))
-# SSE = Sum((10**log10_number_of_cycles - 10**(slope + intercept * log10_stress_parameter))**2)
-# merge = pd.merge(df, results, on=["stress_ratio"])
-# merge["sse"]
-# sse = []
-# for stress_ratio in df.stress_ratio.unique():
+# Export dataframes to files
+snc_output_json_df.to_json(OUTPUT_JSON_FILE, orient="records")
 
-#     sse[stress_ratio] = ((
-#         10**df.set_index("stress_ratio")["log10_number_of_cycles"][stress_ratio]
-#         - 10 ** (results..slope + merge.intercept * merge.log10_stress_parameter_x)
-#     ) ** 2)
-# results["sse"] = merge[["stress_ratio", "sse"]].groupby("stress_ratio").sum()["sse"]
-
-# .groupby("stress_ratio").sum()
-
-
-# SST
-# RSQ
-
-# # For each data, calculate
-# #   P1 = (log10_stress_parameter - mean(log10_stress_parameter))
-# #      * (log10_number_of_cycles - mean(log10_number_of_cycles))
-# df["diff_stress_param"] = df.log10_stress_parameter - df.groupby(
-#     ["stress_ratio"]
-# ).log10_stress_parameter.transform("mean")
-
-
-# df["diff_nb_cycles"] = df.log10_number_of_cycles - df.groupby(
-#     ["stress_ratio"]
-# ).log10_number_of_cycles.transform("mean")
-# df["XY"] = df.diff_stress_param * df.diff_nb_cycles
-
-
-# # avg_by_stress_ratio["P"] = df.groupby(["stress_ratio"]).XY.sum()
-# avg_by_stress_ratio["P"] = df.groupby(["stress_ratio"]).XY.sum()
-
-# df["diff_stress_param**2"] = (
-#     df.diff_stress_param**2
-# )  # ** 2.groupby(["stress_ratio"]).diff_stress_param ** 2.sum()
-
-# avg_by_stress_ratio["Q"] = df.groupby(["stress_ratio"])["diff_stress_param**2"].sum()
-
-# avg_by_stress_ratio["B"] = avg_by_stress_ratio.P / avg_by_stress_ratio.Q
-
-# avg_by_stress_ratio["A"] = (
-#     avg_by_stress_ratio.log10_number_of_cycles
-#     - avg_by_stress_ratio.B * avg_by_stress_ratio.log10_stress_parameter
-# )
-
-# df["Ycar"] = df.log10_stress_parameter * avg_by_stress_ratio.B + avg_by_stress_ratio.A
-
-# avg_by_stress_ratio["AA"] = 10 ** (-avg_by_stress_ratio.A / avg_by_stress_ratio.B)
-
-# avg_by_stress_ratio["BB"] = -1 / avg_by_stress_ratio.B
-
-
-# Create JSON Output file
-# R(f) = avg_by_stress_ratio.stress_ratio
-# 50 constant ???
-# AA
-# BB
-# LRSQ = 1 - LSSE / LSST
-# Fp
-# Linearcheck
-# RMSE
-# SSE
-# SST
-# RSQ
-
-# with open(OUTPUT_JSON_FILE, "w") as f:
-#     json.dump(avg_by_stress_ratio.to_dict(orient="list", axis=1), f)
-# results.to_dict(orient="list", axis=1)
+snc_output_csv_df.to_csv(
+    OUTPUT_CSV_FILE,
+    index=False,
+)
