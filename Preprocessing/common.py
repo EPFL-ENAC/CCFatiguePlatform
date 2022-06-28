55fa9f91d16e62aeab979893f1beb7030a82baff
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-06-28 14:41:20 +0200

    feat(preprocessing): tst_metadata_excel_to_json.py + tst_data_quality_control.py
    can be run only with make run

---
+++ Preprocessing/common.py
@@ -0,0 +1,203 @@
+"""
+Common functions for preprocessing
+"""
+
+import os
+import re
+import glob
+import numpy as np
+
+
+EXPERIMENTS_FOLDER = os.path.abspath(f"{__file__}/../../Data")
+EXPERIMENT_FP_FOLDERS = sorted(
+    filter(lambda fp: os.path.isdir(fp), glob.glob(f"{EXPERIMENTS_FOLDER}/TST_*"))
+)
+
+
+class NotAnExperimentFolder(Exception):
+    pass
+
+
+class Logger:
+    class Indent:
+        def __init__(self):
+            self.current_indent = 0
+
+        def __enter__(self):
+            self.current_indent += 1
+            return self
+
+        def __exit__(self, typ, value, traceback):
+            self.current_indent -= 1
+
+    def __init__(self, filename=None):
+        self.filename = filename
+
+    def __enter__(self):
+        if self.filename is not None:
+            self.f_handler = open(self.filename, "w")
+        self.indent = Logger.Indent()
+        return self
+
+    def __exit__(self, typ, value, traceback):
+        if self.filename is not None:
+            self.f_handler.close()
+
+    def _write(self, messages=[], end="\n"):
+        message = end.join(messages)
+        if self.filename is not None:
+            self.f_handler.write(f"{message}{end}")
+            # self.f_handler.flush()
+        print(message, end=end)
+
+    def write(self, message="", prefix="", end="\n"):
+        indent = " " * self.indent.current_indent
+        lines = [f"{indent}{prefix}{line}" for line in message.split("\n")]
+        self._write(lines, end)
+
+    def info(self, message="", end="\n"):
+        self.write(message=message, prefix="(i): ", end=end)
+
+    def warning(self, message="", end="\n"):
+        self.write(message=message, prefix="Warning: ", end=end)
+
+    def error(self, message="", end="\n"):
+        self.write(message=message, prefix="ERROR: ", end=end)
+
+
+def check_int_column(df, column_name):
+    """
+    Check that all values of a specified column has int values
+    """
+    try:
+        return (
+            df[column_name].replace({np.NaN: 0})
+            - df[column_name].replace({np.NaN: 0}).astype("int64")
+            < 0.000001
+        ).all()
+    except ValueError:
+        return False
+    return True
+
+
+def check_float_column(df, column_name):
+    """
+    Check that all values of a specified column has float values
+    """
+    try:
+        df[column_name].astype("float64")
+    except ValueError:
+        return False
+    return True
+
+
+def check_str_column(df, column_name):
+    """
+    Check that all values of a specified column has str values
+    """
+    try:
+        df[column_name].astype("str")
+    except ValueError:
+        return False
+    return True
+
+
+def check_exist_in_dict(dic, keys):
+    try:
+        obj = dic
+        for key in keys:
+            obj = obj[key]
+        return True
+    except KeyError:
+        return False
+
+
+def check_str_in_dict(dic, keys):
+    try:
+        obj = dic
+        for key in keys:
+            obj = obj[key]
+        return type(obj) == str
+    except KeyError:
+        return False
+
+
+def check_bool_in_dict(dic, keys):
+    try:
+        obj = dic
+        for key in keys:
+            obj = obj[key]
+        return type(obj) == bool
+    except KeyError:
+        return False
+
+
+def get_experiment_meta(experiment_fp_folder):
+    """
+    return following dict
+    {
+        "folder": folder basename
+        "researcher_last_name": experiment's researcher_last_name
+        "date": experiment's date
+        "test_type": experiment's test_type
+        "metadata_xls_fp": full path to metadata xls(x)
+        "csv_fps": full path list to csv files
+        "metadata_json_fp": full path to experiment metadata json (produced from xls)
+        "metadata_csv_fp": full path to test metadata csv (produced from xls)
+    }
+    """
+
+    experiment = {}
+    experiment["folder"] = os.path.basename(experiment_fp_folder)
+    experiment["preprocessed_folder"] = os.path.join(
+        experiment_fp_folder, "preprocessed"
+    )
+    m = re.match("TST_([^_]+)_([0-9-]+)_(FA|QS)", experiment["folder"])
+    if not m:
+        raise NotAnExperimentFolder(experiment_fp_folder)
+    (
+        experiment["researcher_last_name"],
+        experiment["date"],
+        experiment["test_type"],
+    ) = m.groups()
+
+    # search metadata XLS(X) file
+    filename_without_ext = (
+        f"TST_{experiment['date']}_{experiment['test_type']}_metadata"
+    )
+    for extension in ("xls", "xlsx"):
+        metadata_file = os.path.join(
+            experiment_fp_folder,
+            f"{filename_without_ext}.{extension}",
+        )
+        if os.path.exists(metadata_file):
+            experiment["metadata_xls_fp"] = metadata_file
+            experiment["metadata_json_fp"] = os.path.join(
+                experiment["preprocessed_folder"], f"{filename_without_ext}.json"
+            )
+            experiment["metadata_csv_fp"] = os.path.join(
+                experiment["preprocessed_folder"], f"{filename_without_ext}.csv"
+            )
+            break
+    else:
+        experiment["metadata_xls_fp"] = None
+        experiment["metadata_json_fp"] = None
+        experiment["metadata_csv_fp"] = None
+
+    # search CSV files
+    experiment["csv_fps"] = sorted(
+        filter(
+            lambda filename: re.search(
+                f"TST_{experiment['date']}_{experiment['test_type']}_([0-9]+).csv",
+                filename,
+            ),
+            glob.glob(
+                os.path.join(
+                    experiment_fp_folder,
+                    f"TST_{experiment['date']}_{experiment['test_type']}_*.csv",
+                )
+            ),
+        )
+    )
+
+    return experiment
---
+++ Preprocessing/tst_data_quality_control.py
@@ -0,0 +1,216 @@
+"""
+Validate data quality in TST experiment folders
+  + check XLS metadata file and show :
+    + ERROR when file is not found
+
+  + check CSV TST files and show :
+    + ERROR when :
+      + mandatory column is not found
+      + unexpected type is found in a column
+    + WARNING when :
+      + unexpected column found
+"""
+
+#
+# TODO :
+# + check JSON files (from XLS)
+#
+
+import os
+import glob
+import pandas as pd
+import simplejson as json
+import common
+
+
+def check_experiment(experiment_fp_folder):
+    """
+    check requirements for an experiment data + experiment metadata (incl tests)
+    """
+    FIELD_TYPE_CHECK = {
+        str: common.check_str_in_dict,
+        bool: common.check_bool_in_dict,
+    }
+
+    exp = common.get_experiment_meta(experiment_fp_folder)
+
+    logger.write("")
+    logger.write(f"Checking experiment {experiment_fp_folder}")
+    with logger.indent:
+        # Check XLS file
+        if exp["metadata_xls_fp"] is not None:
+            logger.info(f"metadata found : {exp['metadata_xls_fp']}")
+        else:
+            logger.error("metadata file not found !")
+
+        # Check experiment metadata from JSON
+        logger.info(f"Checking {exp['metadata_json_fp']}")
+        with logger.indent:
+            with open(exp["metadata_json_fp"], "r") as f:
+                exp_metadata = json.load(f)
+            # Mandatory fields
+            for field in (
+                {"path": ("Experiment", "General", "Laboratory"), "type": str},
+                {"path": ("Experiment", "General", "Researcher"), "type": str},
+                {"path": ("Experiment", "General", "Experiment Type"), "type": str},
+                {"path": ("Experiment", "General", "Fracture"), "type": bool},
+            ):
+                if not common.check_exist_in_dict(exp_metadata, field["path"]):
+                    logger.error(
+                        f"Mandatory field {'>'.join(field['path'])} is missing"
+                    )
+                elif not FIELD_TYPE_CHECK[field["type"]](exp_metadata, field["path"]):
+                    logger.error(f"Wrong type for field {'>'.join(field['path'])}")
+
+        # check experiment data CSV files
+        for test_data_fp in sorted(
+            glob.glob(
+                os.path.join(
+                    experiment_fp_folder, f"TST_{exp['date']}_{exp['test_type']}_*.csv"
+                )
+            )
+        ):
+            check_test_data_csv(test_data_fp)
+
+
+def check_test_data_csv(test_data_fp):
+    """
+    check requirements for an experiment test data csv file
+    """
+    EXPECTED_COLUMNS = {
+        "Machine_N_cycles": {
+            "type": int,
+            "mandatory": True,
+        },
+        "Machine_Load": {
+            "type": float,
+            "mandatory": True,
+        },
+        "Machine_Displacement": {
+            "type": float,
+            "mandatory": True,
+        },
+        "index": {
+            "type": int,
+            "mandatory": False,
+        },
+        "Camera_N_cycles": {
+            "type": int,
+            "mandatory": False,
+        },
+        "exx": {
+            "type": float,
+            "mandatory": False,
+        },
+        "eyy": {
+            "type": float,
+            "mandatory": False,
+        },
+        "exy": {
+            "type": float,
+            "mandatory": False,
+        },
+        "crack_length": {
+            "type": float,
+            "mandatory": False,
+        },
+        "Th_time": {
+            "type": int,
+            "mandatory": False,
+        },
+        "Th_N_cycles": {
+            "type": int,
+            "mandatory": False,
+        },
+        "Th_specimen_max": {
+            "type": float,
+            "mandatory": False,
+        },
+        "Th_specimen_mean": {
+            "type": float,
+            "mandatory": False,
+        },
+        "Th_chamber": {
+            "type": float,
+            "mandatory": False,
+        },
+        "Th_uppergrips": {
+            "type": float,
+            "mandatory": False,
+        },
+        "Th_lowergrips": {
+            "type": float,
+            "mandatory": False,
+        },
+        "Specimen Name": {
+            "type": str,
+            "mandatory": False,
+        },
+    }
+    COLUMN_TYPE_CHECK = {
+        int: common.check_int_column,
+        float: common.check_float_column,
+        str: common.check_str_column,
+    }
+    TYPE_NAMES = {
+        int: "integer",
+        float: "float",
+        str: "string",
+    }
+    tst = {
+        "fp": test_data_fp,
+        "filename": os.path.basename(test_data_fp),
+    }
+
+    logger.write(f"Checking {tst['filename']}")
+    with logger.indent:
+        dataset = pd.read_csv(
+            test_data_fp,
+            sep=",",
+            header=0,
+        )
+
+        found_columns = dataset.columns
+        for mandatory_col in filter(
+            lambda c: EXPECTED_COLUMNS[c]["mandatory"], EXPECTED_COLUMNS
+        ):
+            if mandatory_col not in found_columns:
+                logger.error(f"mandatory column not found: '{mandatory_col}'")
+            else:
+                if dataset[mandatory_col].isnull().values.any():
+                    logger.error(
+                        f"mandatory column has empty values: '{mandatory_col}'"
+                    )
+        for optional_col in filter(
+            lambda c: not EXPECTED_COLUMNS[c]["mandatory"], EXPECTED_COLUMNS
+        ):
+            if optional_col not in found_columns:
+                logger.info(f"optional column not found: '{optional_col}'")
+        for found_col in found_columns:
+            if found_col not in EXPECTED_COLUMNS:
+                logger.warning(f"unexpected column found: '{found_col}'")
+
+        for col in EXPECTED_COLUMNS:
+            if col in found_columns:
+                if not COLUMN_TYPE_CHECK[EXPECTED_COLUMNS[col]["type"]](dataset, col):
+                    logger.error(
+                        f"column '{col}' is expected to be"
+                        f" of type '{TYPE_NAMES[EXPECTED_COLUMNS[col]['type']]}'"
+                    )
+
+        if dataset.isnull().all(axis=1).any():
+            logger.error("found empty rows")
+
+
+if __name__ == "__main__":
+    for experiment_fp_folder in common.EXPERIMENT_FP_FOLDERS:
+        try:
+            with common.Logger(
+                os.path.join(
+                    experiment_fp_folder,
+                    os.path.basename(experiment_fp_folder) + "_check_output.txt",
+                )
+            ) as logger:
+                check_experiment(experiment_fp_folder)
+        except common.NotAnExperimentFolder:
+            pass
