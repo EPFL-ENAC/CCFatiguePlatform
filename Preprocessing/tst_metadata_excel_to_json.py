20d4be4052b3f4c9b250260453a9e4de7521ce0b
Samuel Bancal <Samuel.Bancal@epfl.ch>
2022-06-29 09:27:32 +0200

    feat(preprocessing): data cleanup

--- Preprocessing/tst_metadata_excel_to_json.py
+++ Preprocessing/tst_metadata_excel_to_json.py
@@ -3,6 +3,8 @@ Writes TST metadata JSON and CSV files from TST metadata Excel file.
 """
 
 import os
+import re
+import numpy as np
 import pandas as pd
 import simplejson as json
 import common
@@ -35,12 +37,86 @@ def df_to_nested_dict(df: pd.DataFrame) -> dict:
 def dict_typo_fix(dic):
     """
     Recursively clean dictionary
+    + keys :
+        + strip white + \n
+    + values :
+        + strip white + \n
+        + remove unexpected "Unnamed: *_level_*"
     """
-    for k, v in dic.items():
-        if type(v) == dict:
+    for k, v in list(dic.items()):
+        new_k = k.strip(" \n")
+        if new_k != k:
+            dic[new_k] = dic.pop(k)
+            k = new_k
+        if re.match(r"Unnamed: \d+_level_\d+", k):
+            del dic[k]
+        elif type(v) == dict:
             dict_typo_fix(v)
         elif type(v) == str:
-            dic[k] = v.strip()
+            dic[k] = v.strip(" \n")
+
+
+def to_bool(value):
+    """
+    transform string value to boolean
+    """
+    if type(value) == bool:
+        return value
+    if value.lower().startswith("y"):
+        return True
+    elif value.lower().startswith("n"):
+        return False
+    else:
+        raise ValueError
+
+
+def meta_dict_fix_types(meta):
+    """
+    Turn expected fields into bool / float (double) / int
+    """
+    # Booleans
+    for (k1, k2, k3) in (("Experiment", "General", "Fracture"),):
+        try:
+            meta[k1][k2][k3] = to_bool(meta[k1][k2][k3])
+        except KeyError:
+            pass
+        except ValueError as e:
+            logger.error(f"exception: {e}")
+
+    # Double
+    for (k1, k2, k3) in (
+        ("Experiment", "General", "Initial Crack length"),
+        ("Experiment", "General", "Reliability Level"),
+        ("Experiment", "Material Type", "Area Density"),
+        ("Experiment", "Geometry", "Length"),
+        ("Experiment", "Geometry", "Width"),
+        ("Experiment", "Geometry", "Thickness"),
+        ("Experiment", "Laminates and Assemblies", "Curing Time"),
+        ("Experiment", "Laminates and Assemblies", "Curing Temperature"),
+        ("Experiment", "Laminates and Assemblies", "Curing Pressure"),
+        ("Experiment", "Laminates and Assemblies", "Fiber Content"),
+        ("Experiment", "Test condtions", "Temperature"),
+        ("Experiment", "Test condtions", "Humidity"),
+    ):
+        try:
+            meta[k1][k2][k3] = float(meta[k1][k2][k3])
+        except KeyError:
+            pass
+        except ValueError as e:
+            logger.error(f"exception: {e}")
+
+    # Int
+    for (k1, k2, k3) in (
+        ("Experiment", "DIC Analysis", "Subset Size"),
+        ("Experiment", "DIC Analysis", "Step Size"),
+    ):
+        try:
+            if not np.isnan(meta[k1][k2][k3]):
+                meta[k1][k2][k3] = int(meta[k1][k2][k3])
+        except KeyError:
+            pass
+        except ValueError as e:
+            logger.error(f"exception: {e}")
 
 
 if __name__ == "__main__":
@@ -56,12 +132,16 @@ if __name__ == "__main__":
                 # Experiment tab to JSON metadata
                 # - - - - - - - - - - - - - - - -
                 contents_df = pd.read_excel(
-                    exp["metadata_xls_fp"], sheet_name="Experiment", header=[0, 1]
+                    exp["metadata_xls_fp"],
+                    sheet_name="Experiment",
+                    header=[0, 1],
                 )
                 contents_dict = {}
                 contents_dict["Experiment"] = df_to_nested_dict(contents_df)[0]
 
-                # Fix typos
+                # Cleanup
+                dict_typo_fix(contents_dict)
+                meta_dict_fix_types(contents_dict)
 
                 os.makedirs(exp["preprocessed_folder"], exist_ok=True)
                 with open(exp["metadata_json_fp"], "w") as f:
