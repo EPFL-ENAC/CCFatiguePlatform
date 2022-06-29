c43489716964ad9dabe9d76ed46d9b5d8a2af75a
Nicolas Dubois <nicolas.dubois@epfl.ch>
2022-06-29 16:29:00 +0200

    :truck: (Harris) Rename file

--- CCFatigue_modules/3_CLD/Harris/cld_harris.py
+++ CCFatigue_modules/3_CLD/Harris/cld_harris.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """ CCFatigue - Module 3 - CLD-Harris.py
 
-Harris diagram is described in Tassos red book p. 108 - the original papers are 
-https://www.sciencedirect.com/science/article/pii/0142112394904782?via%3Dihub (https://doi.org/10.1016/0142-1123(94)90478-2)
-https://www.sciencedirect.com/science/article/pii/S0266353897001218?via%3Dihub (https://doi.org/10.1016/S0266-3538(97)00121-8)
+Harris diagram is described in Tassos red book p. 108 - the original papers are:
+- https://www.sciencedirect.com/science/article/pii/0142112394904782?via%3Dihub
+  (https://doi.org/10.1016/0142-1123(94)90478-2)
+- https://www.sciencedirect.com/science/article/pii/S0266353897001218?via%3Dihub
+  (https://doi.org/10.1016/S0266-3538(97)00121-8)
 """
 
 import os
@@ -147,7 +149,7 @@ if __name__ == "__main__":
 
             row = pd.DataFrame(
                 {
-                    "onc": [onc],
+                    "cycles_to_failure": [onc],
                     "stress_amplitude": [stress_amplitude],
                     "mean_stress": [mean_stress],
                 }
