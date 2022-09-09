e81057af643aa42331b977b829d0f6ae10117961
David Tang <david.tang@epfl.ch>
2022-09-09 13:48:15 +0200

    feat: sn curve sendeckyj

--- backend/tests/test_modules.py
+++ backend/tests/test_modules.py
@@ -1,4 +1,7 @@
 import os
+import ccfatigue.modules.sn_curve_loglog as sn_curve_loglog
+import ccfatigue.modules.sn_curve_linlog as sn_curve_linlog
+import ccfatigue.modules.sn_curve_sendeckyj as sn_curve_sendeckyj
 
 SRC_DIR = os.path.dirname(os.path.realpath(__file__))
 DATA_DIR = os.path.join(SRC_DIR, "..", "..", "Data")
@@ -6,30 +9,39 @@ OUTPUT_DIR = os.path.join(DATA_DIR, "output")
 
 
 def test_sn_curve_loglog():
-    import ccfatigue.modules.sn_curve_loglog as sn_curve_loglog
-
     INPUT_FILENAME = "AGG_input.csv"
     OUTPUT_JSON_FILENAME = "SNC_LogLog.json"
     OUTPUT_CSV_FILENAME = "SNC_LogLog.csv"
     INPUT_FILE = os.path.join(DATA_DIR, INPUT_FILENAME)
     OUTPUT_JSON_FILE = os.path.join(OUTPUT_DIR, OUTPUT_JSON_FILENAME)
     OUTPUT_CSV_FILE = os.path.join(OUTPUT_DIR, OUTPUT_CSV_FILENAME)
-    print(f"{INPUT_FILE=}")
-    print(f"{OUTPUT_JSON_FILE=}")
-    print(f"{OUTPUT_CSV_FILE=}")
+    print(f"INPUT_FILE={os.path.abspath(INPUT_FILE)}")
+    print(f"OUTPUT_JSON_FILE={os.path.abspath(OUTPUT_JSON_FILE)}")
+    print(f"OUTPUT_CSV_FILE={os.path.abspath(OUTPUT_CSV_FILE)}")
     sn_curve_loglog.execute(INPUT_FILE, OUTPUT_JSON_FILE, OUTPUT_CSV_FILE)
 
 
 def test_sn_curve_linlog():
-    import ccfatigue.modules.sn_curve_linlog as sn_curve_linlog
-
     INPUT_FILENAME = "AGG_input.csv"
     INPUT_FILE = os.path.join(DATA_DIR, INPUT_FILENAME)
     OUTPUT_JSON_FILENAME = "SNC_LinLog.json"
     OUTPUT_JSON_FILE = os.path.join(OUTPUT_DIR, OUTPUT_JSON_FILENAME)
     OUTPUT_CSV_FILENAME = "SNC_LinLog.csv"
     OUTPUT_CSV_FILE = os.path.join(OUTPUT_DIR, OUTPUT_CSV_FILENAME)
-    print(f"{INPUT_FILE=}")
-    print(f"{OUTPUT_JSON_FILE=}")
-    print(f"{OUTPUT_CSV_FILE=}")
+    print(f"INPUT_FILE={os.path.abspath(INPUT_FILE)}")
+    print(f"OUTPUT_JSON_FILE={os.path.abspath(OUTPUT_JSON_FILE)}")
+    print(f"OUTPUT_CSV_FILE={os.path.abspath(OUTPUT_CSV_FILE)}")
     sn_curve_linlog.execute(INPUT_FILE, OUTPUT_JSON_FILE, OUTPUT_CSV_FILE)
+
+
+def test_sn_curve_sendeckyj():
+    INPUT_FILENAME = "AGG_input.csv"
+    INPUT_FILE = os.path.join(DATA_DIR, INPUT_FILENAME)
+    OUTPUT_JSON_FILENAME = "SNC_Sendeckyj.json"
+    OUTPUT_JSON_FILE = os.path.join(OUTPUT_DIR, OUTPUT_JSON_FILENAME)
+    OUTPUT_CSV_FILENAME = "SNC_Sendeckyj.csv"
+    OUTPUT_CSV_FILE = os.path.join(OUTPUT_DIR, OUTPUT_CSV_FILENAME)
+    print(f"INPUT_FILE={os.path.abspath(INPUT_FILE)}")
+    print(f"OUTPUT_JSON_FILE={os.path.abspath(OUTPUT_JSON_FILE)}")
+    print(f"OUTPUT_CSV_FILE={os.path.abspath(OUTPUT_CSV_FILE)}")
+    sn_curve_sendeckyj.execute(INPUT_FILE, OUTPUT_JSON_FILE, OUTPUT_CSV_FILE)
