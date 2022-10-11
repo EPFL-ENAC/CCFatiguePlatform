5f0c352c84caa705a477e8d95d7e99b4ce9adbbe
David Tang <david.tang@epfl.ch>
2022-10-11 15:06:24 +0200

    feat(dashboard): add quasi-static third graph #120

--- backend/ccfatigue/experiment/quasi_static.py
+++ backend/ccfatigue/experiment/quasi_static.py
@@ -17,6 +17,8 @@ class QuasiStaticTest(BaseModel):
     crack_displacement: List[float]
     crack_load: List[float]
     crack_length: List[float]
+    strain: Dict[str, List[float]]
+    stress: List[float]
 
 
 def get_dataframe(
@@ -37,6 +39,25 @@ def get_dataframe(
     return pd.read_csv(abspath)
 
 
+def get_test_metadata(
+    exp: Dict[str, str],
+    specimen_id: int,
+) -> Dict:
+    """
+    return extracted metadata related to the test from CSV
+    """
+    # FIXME researcher_name from a column value
+    researcher_name = exp["researcher"].split(" ")[-1]
+    filepath = os.path.join(
+        DATA_DIRECTORY,
+        f"TST_{researcher_name}_{exp['date']}_{exp['experiment_type']}",
+        "tests.csv",
+    )
+    abspath = os.path.abspath(filepath)
+    df = pd.read_csv(abspath)
+    return df[df["specimen number"] == specimen_id].to_dict("records")[0]
+
+
 async def quasi_static_test(
     session: AsyncSession,
     experiment_id: int,
@@ -49,6 +70,7 @@ async def quasi_static_test(
                     Experiment.researcher,
                     Experiment.experiment_type,
                     Experiment.date,
+                    Experiment.fracture,
                 ).where(Experiment.id == experiment_id)
             )
         )
@@ -57,12 +79,38 @@ async def quasi_static_test(
     )
     specimen_id = await get_specimen_id(session, experiment_id, test_id)
     df = get_dataframe(experiment, specimen_id)
-    machine_df = df[["Machine_Displacement", "Machine_Load"]].dropna()
-    crack_df = df[["Crack_Displacement", "Crack_Load", "Crack_length"]].dropna()
+    machine_df = (
+        df[["Machine_Displacement", "Machine_Load"]].dropna()
+        if {"Machine_Displacement", "Machine_Load"}.issubset(df.columns)
+        else pd.DataFrame(columns=["Machine_Displacement", "Machine_Load"])
+    )
+
+    fracture = experiment["fracture"]
+    crack_df = (
+        df[["Crack_Displacement", "Crack_Load", "Crack_length"]].dropna()
+        if fracture
+        else pd.DataFrame(columns=["Crack_Displacement", "Crack_Load", "Crack_length"])
+    )
+    strain_stress_df = pd.DataFrame(columns=["Stress"])
+    strain: Dict[str, List[float]] = {}
+    if not fracture:
+        test = get_test_metadata(experiment, specimen_id)
+        area = test["width"] * test["thickness"]
+
+        if "exx--1" in df.columns:
+            strain["exx--1"] = df["exx--1"].to_list()
+        if "eyy--1" in df.columns:
+            strain["eyy--1"] = df["eyy--1"].to_list()
+        if "exy--1" in df.columns:
+            strain["exy--1"] = df["exy--1"].to_list()
+
+        strain_stress_df["Stress"] = df["MD_Load--1"] / area
     return QuasiStaticTest(
         machine_displacement=machine_df["Machine_Displacement"].to_list(),
         machine_load=machine_df["Machine_Load"].to_list(),
         crack_displacement=crack_df["Crack_Displacement"].to_list(),
         crack_load=crack_df["Crack_Load"].to_list(),
         crack_length=crack_df["Crack_length"].to_list(),
+        strain=strain,
+        stress=strain_stress_df["Stress"].to_list(),
     )
