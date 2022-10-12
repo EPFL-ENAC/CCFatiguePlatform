93517c471187839845b895a1bea8889d9fec9691
Nicolas Dubois <nicolas.dubois@epfl.ch>
2022-10-12 10:56:05 +0200

    fix(mod 3 harris): fix missing rows output
    FIX #129

--- backend/ccfatigue/modules/cld_harris.py
+++ backend/ccfatigue/modules/cld_harris.py
@@ -12,10 +12,10 @@ Harris diagram is described in Tassos red book p. 108 - the original paper is:
 
 import numpy as np
 import pandas as pd
-from pandas._typing import ReadCsvBuffer, WriteBuffer, FilePath
+from pandas._typing import FilePath, ReadCsvBuffer, WriteBuffer
 from scipy import stats
-import ccfatigue.modules.harris_utils as harris_utils
 
+import ccfatigue.modules.harris_utils as harris_utils
 
 # staticvalue.txt => constants
 # ucs = 27.1
@@ -25,6 +25,40 @@ import ccfatigue.modules.harris_utils as harris_utils
 
 # Cycles for the isolines (the lines of the CLD)
 CLD_CYCLES_COUNT = [10**x for x in range(3, 10)]  # = 1e3, 1e4, ..., 1e9
+
+
+def cld_add_row(
+    cld_df, cycles_to_failure: float, stress_amplitude: float, stress_mean: float
+) -> pd.DataFrame:
+    """
+    Add row to CLD DataFrame
+    Parameters
+    ----------
+        cld_df
+            CLD DataFrame
+        cycles_to_failure: float
+        stress_amplitude: float
+        stress_mean: float
+    Returns
+    -------
+        CLD DataFrame
+    """
+    row = pd.DataFrame(
+        {
+            "cycles_to_failure": [cycles_to_failure],
+            "stress_amplitude": [stress_amplitude],
+            "stress_mean": [stress_mean],
+        }
+    )
+
+    cld_df = pd.concat(
+        [
+            cld_df,
+            row,
+        ],
+        ignore_index=True,
+    )
+    return cld_df
 
 
 def execute(
@@ -49,17 +83,17 @@ def execute(
     bounds_margin = harris_utils.BOUNDS_MARGIN
 
     # Import input files (SNC format)
-    SNC_df = pd.read_csv(input_file)
+    snc_df = pd.read_csv(input_file)
 
     # Data are grouped by stress_ratio but one experiment
     # can have two separate groups with same stress_ratio so we need to identify
-    SNC_df["stress_ratio_id"] = (
-        SNC_df.stress_ratio != SNC_df.stress_ratio.shift()
+    snc_df["stress_ratio_id"] = (
+        snc_df.stress_ratio != snc_df.stress_ratio.shift()
     ).cumsum()
 
     # Average by stress_ratio
     stress_ratios_df = (
-        SNC_df[
+        snc_df[
             [
                 "stress_ratio_id",
                 "stress_ratio",
@@ -74,19 +108,19 @@ def execute(
         raise Exception("Input data is not enough to apply Haris' method.")
 
     # Calculate sigma amplitude
-    SNC_df["stress_amplitude"] = SNC_df.apply(
+    snc_df["stress_amplitude"] = snc_df.apply(
         lambda x: harris_utils.calculate_stress_amplitude(x.stress_ratio, x.stress_max),
         axis=1,
     )
 
     # Calculate mean sigma
-    SNC_df["stress_mean"] = SNC_df.apply(
+    snc_df["stress_mean"] = snc_df.apply(
         lambda x: harris_utils.calculate_stress_mean(x.stress_ratio, x.stress_max),
         axis=1,
     )
 
     # Apply bounds to mean sigma (applied to module 3, not to module 5)
-    SNC_df["stress_mean"] = SNC_df.apply(
+    snc_df["stress_mean"] = snc_df.apply(
         lambda x: harris_utils.bounds_stress_mean(
             x.stress_mean,
             uts,
@@ -96,20 +130,20 @@ def execute(
         axis=1,
     )
 
-    SNC_df["y"] = np.log10(SNC_df.stress_amplitude / uts)
-    SNC_df["x1"] = np.log10(1 - (SNC_df.stress_mean / uts))
-    SNC_df["x2"] = np.log10(ucs / uts + SNC_df.stress_mean / uts)
+    snc_df["y"] = np.log10(snc_df.stress_amplitude / uts)
+    snc_df["x1"] = np.log10(1 - (snc_df.stress_mean / uts))
+    snc_df["x2"] = np.log10(ucs / uts + snc_df.stress_mean / uts)
 
     cycles_df = pd.DataFrame(
-        SNC_df.cycles_to_failure.unique(), columns=["cycles_to_failure"]
+        snc_df.cycles_to_failure.unique(), columns=["cycles_to_failure"]
     )
 
     # Log10(N)
     cycles_df["log10_cycles_to_failure"] = np.log10(cycles_df.cycles_to_failure)
 
-    for cycles_count in SNC_df.cycles_to_failure.unique():
+    for cycles_count in snc_df.cycles_to_failure.unique():
 
-        cycle_df = SNC_df.loc[SNC_df.cycles_to_failure == cycles_count]
+        cycle_df = snc_df.loc[snc_df.cycles_to_failure == cycles_count]
 
         f, u, v = harris_utils.calculate_fuv(
             cycle_df.x1.to_numpy(), cycle_df.x2.to_numpy(), cycle_df.y.to_numpy()
@@ -124,42 +158,51 @@ def execute(
     linregress_u = stats.linregress(cycles_df.log10_cycles_to_failure, cycles_df.u)
     linregress_v = stats.linregress(cycles_df.log10_cycles_to_failure, cycles_df.v)
 
-    CLD_df = pd.DataFrame()
+    cld_df = pd.DataFrame()
 
-    for onc in CLD_CYCLES_COUNT:
+    for cycles_to_failure in CLD_CYCLES_COUNT:
 
         # Eq 8 p530 ref[1]
-        ff = linregress_f.slope * np.log10(onc) + linregress_f.intercept  # type: ignore # noqa
-        uu = linregress_u.slope * np.log10(onc) + linregress_u.intercept  # type: ignore # noqa
-        vv = linregress_v.slope * np.log10(onc) + linregress_v.intercept  # type: ignore # noqa
+        ff = linregress_f.slope * np.log10(cycles_to_failure) + linregress_f.intercept  # type: ignore # noqa
+        uu = linregress_u.slope * np.log10(cycles_to_failure) + linregress_u.intercept  # type: ignore # noqa
+        vv = linregress_v.slope * np.log10(cycles_to_failure) + linregress_v.intercept  # type: ignore # noqa
 
+        cld_df = cld_add_row(
+            cld_df=cld_df,
+            cycles_to_failure=cycles_to_failure,
+            stress_amplitude=0,
+            stress_mean=-ucs,
+        )
+
+        increment = 0.90 * (uts + ucs) / 40
+        start = -0.90 * ucs
+        stop = 0.90 * uts + increment
         for sm in np.arange(
-            -0.90 * ucs,
-            0.90 * uts,
-            0.90 * (uts + ucs) / 40,
+            start,
+            stop,
+            increment,
         ):
 
-            C2 = ff * (1 - sm / uts) ** uu
-            C3 = ((ucs / uts) + (sm / uts)) ** vv
+            # https://github.com/EPFL-ENAC/CCFatiguePlatform/blob/develop/CCFatigue_modules/3_CLD/Harris/CLD-Harris.for#L192
+            c2 = ff * (1 - sm / uts) ** uu
+            c3 = ((ucs / uts) + (sm / uts)) ** vv
 
-            stress_amplitude = C2 * C3 * uts
+            stress_amplitude = c2 * c3 * uts
             stress_mean = sm
 
-            row = pd.DataFrame(
-                {
-                    "cycles_to_failure": [onc],
-                    "stress_amplitude": [stress_amplitude],
-                    "stress_mean": [stress_mean],
-                }
+            cld_df = cld_add_row(
+                cld_df=cld_df,
+                cycles_to_failure=cycles_to_failure,
+                stress_amplitude=stress_amplitude,
+                stress_mean=stress_mean,
             )
 
-            CLD_df = pd.concat(
-                [
-                    CLD_df,
-                    row,
-                ],
-                ignore_index=True,
-            )
+        cld_df = cld_add_row(
+            cld_df=cld_df,
+            cycles_to_failure=cycles_to_failure,
+            stress_amplitude=0,
+            stress_mean=uts,
+        )
 
     # Generate output files
-    CLD_df.to_csv(path_or_buf=output_csv_file, index=False)  # type: ignore
+    cld_df.to_csv(path_or_buf=output_csv_file, index=False)  # type: ignore
