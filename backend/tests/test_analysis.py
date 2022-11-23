21769f1f9e26c78c42b6f6164c791360cb26c007
Nicolas Dubois <nicolas.dubois@epfl.ch>
2022-11-23 13:37:45 +0100

    feat(analysis): add mod3+5 piecewise linear algo
    FIX 175

--- backend/tests/test_analysis.py
+++ backend/tests/test_analysis.py
@@ -1,9 +1,11 @@
 import os
 
 import ccfatigue.analysis.cld_harris as cld_harris
+import ccfatigue.analysis.cld_piecewiselinear as cld_piecewiselinear  # noqa: E501
 import ccfatigue.analysis.cyc_range_mean as cyc_range_mean
+import ccfatigue.analysis.das_piecewiselinear as das_piecewiselinear  # noqa: E501
 import ccfatigue.analysis.faf_ftpf as faf_ftpf
-import ccfatigue.analysis.miner_harris as miner_harris
+import ccfatigue.analysis.miner_harris as das_harris
 import ccfatigue.analysis.sn_curve_linlog as sn_curve_linlog
 import ccfatigue.analysis.sn_curve_loglog as sn_curve_loglog
 import ccfatigue.analysis.sn_curve_sendeckyj as sn_curve_sendeckyj
@@ -19,28 +21,36 @@ SNC_CSV_2_INPUT_FILE = os.path.join(DATA_DIR, "SNC_input2.csv")
 SNC_JSON_X_INPUT_FILE = os.path.join(DATA_DIR, "SNC_inputX.json")
 SNC_JSON_Y_INPUT_FILE = os.path.join(DATA_DIR, "SNC_inputY.json")
 SNC_JSON_F_INPUT_FILE = os.path.join(DATA_DIR, "SNC_inputF.json")
+CYC_CSV_CCINPUT_1_INPUT_FILE = os.path.join(DATA_DIR, "CYC_ccinput.csv")
 CYC_CSV_CCINPUT_2_INPUT_FILE = os.path.join(DATA_DIR, "CYC_ccinput2.csv")
 LDS_CSV_1_INPUT_FILE = os.path.join(DATA_DIR, "LDS_input.csv")
 
 # Mod 1 - Cycle counting - Range mean
-RANGE_MEAN_CYC_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "CYC_range_mean.csv")
+CYC_RANGE_MEAN_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "CYC_range_mean.csv")
 
 # Mod 2 - S-N Curve - LogLog
-LOGLOG_SNC_JSON_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LogLog.json")
-LOGLOG_SNC_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LogLog.csv")
+SNC_LOGLOG_JSON_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LogLog.json")
+SNC_LOGLOG_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LogLog.csv")
 
 # Mod 2 - S-N Curve - LinLog
-LINLOG_SNC_JSON_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LinLog.json")
-LINLOG_SNC_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LinLog.csv")
+SNC_LINLOG_JSON_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LinLog.json")
+SNC_LINLOG_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_LinLog.csv")
 
 # Mod 2 - S-N Curve - Sendeckyj
-SENDECKYJ_SNC_JSON_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_Sendeckyj.json")
-SENDECKYJ_SNC_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_Sendeckyj.csv")
+SNC_SENDECKYJ_JSON_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_Sendeckyj.json")
+SNC_SENDECKYJ_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "SNC_Sendeckyj.csv")
 
 # Mod 3 - Constant life diagram - Harris
 CLD_HARRIS_OUTPUT_CLD_CSV_FILE = os.path.join(OUTPUT_DIR, "CLD_Harris.csv")
 CLD_HARRIS_UCS = 27.1
 CLD_HARRIS_UTS = 27.7
+
+# Mod 3 - Constant life diagram - Piecewise Linear
+CLD_PIECEWISELINEAR_OUTPUT_CSV_FILE = os.path.join(
+    OUTPUT_DIR, "CLD_PiecewiseLinear.csv"
+)
+CLD_PIECEWISELINEAR_UCS = 27.1
+CLD_PIECEWISELINEAR_UTS = 27.7
 
 # Mod 4 - Fatigue failture - FTPF
 FAF_FTPF_CSV_OUTPUT_FILE = os.path.join(OUTPUT_DIR, "FAF_ftpf.csv")
@@ -49,12 +59,22 @@ FAF_FTPF_DESIRABLE_ANGLE = 30
 FAF_FTPF_OFF_AXIS_ANGLE = 160
 
 # Mod 5 - Damage summation - Harris
-MINER_HARRIS_OUTPUT_DAS_CSV_FILE = os.path.join(OUTPUT_DIR, "DAS_harris.csv")
-MINER_HARRIS_UCS = 367.2
-MINER_HARRIS_UTS = 416.5
-MINER_HARRIS_FACTOR_START = 1.4
-MINER_HARRIS_FACTOR_STOP = 0.4
-MINER_HARRIS_FACTOR_STEP = -0.1
+DAS_HARRIS_OUTPUT_DAS_CSV_FILE = os.path.join(OUTPUT_DIR, "DAS_harris.csv")
+DAS_HARRIS_UCS = 367.2
+DAS_HARRIS_UTS = 416.5
+DAS_HARRIS_FACTOR_START = 1.4
+DAS_HARRIS_FACTOR_STOP = 0.4
+DAS_HARRIS_FACTOR_STEP = -0.1
+
+# Mod 5 - Damage summation - Piecewise Linear
+DAS_PIECEWISELINEAR_OUTPUT_DAS_CSV_FILE = os.path.join(
+    OUTPUT_DIR, "DAS_piecewiselinear.csv"
+)
+DAS_PIECEWISELINEAR_UCS = 367.2
+DAS_PIECEWISELINEAR_UTS = 416.5
+DAS_PIECEWISELINEAR_FACTOR_START = 1.4
+DAS_PIECEWISELINEAR_FACTOR_STOP = 0.35
+DAS_PIECEWISELINEAR_FACTOR_STEP = -0.1
 
 os.makedirs(OUTPUT_DIR, exist_ok=True)
 
@@ -63,7 +83,7 @@ os.makedirs(OUTPUT_DIR, exist_ok=True)
 
 
 def test_cyc_range_mean():
-    cyc_range_mean.execute(LDS_CSV_1_INPUT_FILE, RANGE_MEAN_CYC_CSV_OUTPUT_FILE)
+    cyc_range_mean.execute(LDS_CSV_1_INPUT_FILE, CYC_RANGE_MEAN_CSV_OUTPUT_FILE)
 
 
 # Module 2 - S-N curve
@@ -71,21 +91,21 @@ def test_cyc_range_mean():
 
 def test_sn_curve_loglog():
     sn_curve_loglog.execute(
-        AGG_CSV_INPUT_FILE, LOGLOG_SNC_JSON_OUTPUT_FILE, LOGLOG_SNC_CSV_OUTPUT_FILE
+        AGG_CSV_INPUT_FILE, SNC_LOGLOG_JSON_OUTPUT_FILE, SNC_LOGLOG_CSV_OUTPUT_FILE
     )
 
 
 def test_sn_curve_linlog():
     sn_curve_linlog.execute(
-        AGG_CSV_INPUT_FILE, LINLOG_SNC_JSON_OUTPUT_FILE, LINLOG_SNC_CSV_OUTPUT_FILE
+        AGG_CSV_INPUT_FILE, SNC_LINLOG_JSON_OUTPUT_FILE, SNC_LINLOG_CSV_OUTPUT_FILE
     )
 
 
 def test_sn_curve_sendeckyj():
     sn_curve_sendeckyj.execute(
         AGG_CSV_INPUT_FILE,
-        SENDECKYJ_SNC_JSON_OUTPUT_FILE,
-        SENDECKYJ_SNC_CSV_OUTPUT_FILE,
+        SNC_SENDECKYJ_JSON_OUTPUT_FILE,
+        SNC_SENDECKYJ_CSV_OUTPUT_FILE,
     )
 
 
@@ -98,6 +118,15 @@ def test_cld_harris() -> None:
         CLD_HARRIS_OUTPUT_CLD_CSV_FILE,
         CLD_HARRIS_UCS,
         CLD_HARRIS_UTS,
+    )
+
+
+def test_cld_piecewiselinear() -> None:
+    cld_piecewiselinear.execute(
+        SNC_CSV_1_INPUT_FILE,
+        CLD_PIECEWISELINEAR_OUTPUT_CSV_FILE,
+        CLD_PIECEWISELINEAR_UCS,
+        CLD_PIECEWISELINEAR_UTS,
     )
 
 
@@ -120,14 +149,27 @@ def test_faf_ftpf():
 # Module 5 - Damage summation
 
 
-def test_miner_harris() -> None:
-    miner_harris.execute(
+def test_das_harris() -> None:
+    das_harris.execute(
         SNC_CSV_2_INPUT_FILE,
         CYC_CSV_CCINPUT_2_INPUT_FILE,
-        MINER_HARRIS_OUTPUT_DAS_CSV_FILE,
-        MINER_HARRIS_UCS,
-        MINER_HARRIS_UTS,
-        MINER_HARRIS_FACTOR_START,
-        MINER_HARRIS_FACTOR_STOP,
-        MINER_HARRIS_FACTOR_STEP,
+        DAS_HARRIS_OUTPUT_DAS_CSV_FILE,
+        DAS_HARRIS_UCS,
+        DAS_HARRIS_UTS,
+        DAS_HARRIS_FACTOR_START,
+        DAS_HARRIS_FACTOR_STOP,
+        DAS_HARRIS_FACTOR_STEP,
+    )
+
+
+def test_das_piecewiselinear() -> None:
+    das_piecewiselinear.execute(
+        SNC_CSV_2_INPUT_FILE,
+        CYC_CSV_CCINPUT_1_INPUT_FILE,
+        DAS_PIECEWISELINEAR_OUTPUT_DAS_CSV_FILE,
+        DAS_PIECEWISELINEAR_UCS,
+        DAS_PIECEWISELINEAR_UTS,
+        DAS_PIECEWISELINEAR_FACTOR_START,
+        DAS_PIECEWISELINEAR_FACTOR_STOP,
+        DAS_PIECEWISELINEAR_FACTOR_STEP,
     )
